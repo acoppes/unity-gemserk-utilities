292e23c7e1ec6fc9d97e0a38aae46a2c584ecd72
Ariel Coppes <ariel.coppes@gmail.com>
2022-05-23 10:34:37 -0300

    added target basic stuff

--- Assets/Gemserk.Leopotam.Ecs/Components.cs
+++ Assets/Gemserk.Leopotam.Ecs/Components.cs
@@ -1,8 +1,37 @@
+using UnityEngine;
+
 namespace Gemserk.Leopotam.Ecs
 { 
+    public struct PositionComponent : IEntityComponent
+    {
+        public Vector2 value;
+    }
+    
     public struct NameComponent : IEntityComponent
     {
         public string name;
         public bool singleton;
+    }
+    
+    public struct PlayerComponent : IEntityComponent
+    {
+        public int player;
+    }
+
+    public struct Target
+    {
+        public int entity;
+        public int player;
+        public Vector2 position;
+    }
+
+    // public struct TargetPosition
+    // {
+    //     public Target target;
+    // }
+
+    public struct TargetComponent : IEntityComponent
+    {
+        public Target target;
     }
 }
