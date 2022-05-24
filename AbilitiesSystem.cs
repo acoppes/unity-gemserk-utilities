3e536bf3dabb25374ef62f66e697656e33c682ba
Ariel Coppes <ariel.coppes@gmail.com>
2022-05-24 16:04:46 -0300

    added some reusable systems and ocmponents

---
+++ AbilitiesSystem.cs
@@ -0,0 +1,113 @@
+using Gemserk.Leopotam.Ecs;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace GBJAM9.Ecs
+{
+    public class AbilitiesSystem : BaseSystem, IEcsRunSystem, IEntityCreatedHandler
+    {
+        public void OnEntityCreated(Gemserk.Leopotam.Ecs.World world, int entity)
+        {
+            if (world.HasComponent<AbilitiesComponent>(entity))
+            {
+                var abilitiesComponent = world.GetComponent<AbilitiesComponent>(entity);
+                foreach (var ability in abilitiesComponent.abilities)
+                {
+                    if (ability.startType == Ability.StartType.Loaded)
+                    {
+                        ability.cooldownCurrent = ability.cooldownTotal;
+                    }
+                    else if (ability.startType == Ability.StartType.Unloaded)
+                    {
+                        ability.cooldownCurrent = 0;
+                    }
+                }
+            }
+        }
+        
+        public void Run(EcsSystems systems)
+        {
+            var filter = world.GetFilter<AbilitiesComponent>().End();
+            var positions = world.GetComponents<PositionComponent>();
+            var lookingDirections = world.GetComponents<LookingDirection>();
+
+            var abilitiesComponents = world.GetComponents<AbilitiesComponent>();
+
+            // update ability position
+            foreach (var entity in world.GetFilter<AbilitiesComponent>().Inc<PositionComponent>().End())
+            {
+                var abilitiesComponent = abilitiesComponents.Get(entity);
+                var position = positions.Get(entity);
+                foreach (var ability in abilitiesComponent.abilities)
+                {
+                    // + fire position attach point
+                    ability.position = position.value;
+                }
+            }
+            
+            // Update ability direction
+            foreach (var entity in world.GetFilter<AbilitiesComponent>().Inc<LookingDirection>().End())
+            {
+                var abilitiesComponent = abilitiesComponents.Get(entity);
+                var lookingDirection = lookingDirections.Get(entity);
+                foreach (var ability in abilitiesComponent.abilities)
+                {
+                    ability.direction = lookingDirection.value;
+                }
+            }
+
+            foreach (var entity in filter)
+            {
+                ref var abilitiesComponent = ref abilitiesComponents.Get(entity);
+                foreach (var ability in abilitiesComponent.abilities)
+                {
+                    if (!ability.isRunning)
+                    {
+                        ability.cooldownCurrent += Time.deltaTime;
+                    }
+                    else
+                    {
+                        ability.runningTime += Time.deltaTime;
+                        ability.isComplete = false;
+                    }
+
+                    if (!ability.isComplete && ability.runningTime > ability.duration)
+                    {
+                        // fire projectile?
+
+                        if (ability.projectileDefinition != null)
+                        {
+                            var projectileEntity = world.CreateEntity(ability.projectileDefinition);
+                            ref var projectileComponent = ref world.GetComponent<ProjectileComponent>(projectileEntity);
+
+                            projectileComponent.startPosition = ability.position;
+                            projectileComponent.startDirection = ability.direction;
+                            
+                            // instantiate target effects in projectile
+                            
+                            // Copy player to for damage later
+                            // projectileController.entity.player.player = e.player.player;
+                            
+                            // Copy damage
+                            // projectileEntity.projectile.damage = weaponData.damage + e.attack.extraDamage;
+                            
+                            // this was for collisions
+                            // if (e.player.player == 0)
+                            // {
+                            //     projectileController.gameObject.layer = playerProjectilesLayer;
+                            // }
+                            // else
+                            // {
+                            //     projectileController.gameObject.layer = enemyProjectilesLayer;
+                            // }
+                        }
+
+                        ability.isComplete = true;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- Components.cs
+++ Components.cs
@@ -1,7 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Gemserk.Leopotam.Ecs
 { 
+    public struct LookingDirection : IEntityComponent
+    {
+        public Vector2 value;
+        public bool disableIndicator;
+    }
+    
     public struct PositionComponent : IEntityComponent
     {
         public Vector2 value;
@@ -35,5 +44,90 @@ namespace Gemserk.Leopotam.Ecs
     public struct TargetComponent : IEntityComponent
     {
         public Target target;
+    }
+    
+    public class Ability
+    {
+        public enum StartType
+        {
+            Loaded = 0,
+            Unloaded = 1
+        }
+        
+        public string name;
+        
+        public float duration;
+        
+        public float cooldownTotal;
+        public float cooldownCurrent;
+        
+        public float runningTime;
+        
+        public bool isReady => isCooldownReady && !isRunning;
+        public bool isCooldownReady => cooldownCurrent > cooldownTotal;
+
+        public bool isComplete;
+
+        public bool isRunning;
+
+        public Vector2 position;
+        public Vector2 direction;
+
+        public IEntityDefinition projectileDefinition;
+        
+        public Ability.StartType startType;
+
+        public void StartRunning()
+        {
+            runningTime = 0;
+            isRunning = true;
+            // isComplete = false;
+        }
+
+        public void Stop()
+        {
+            cooldownCurrent = 0;
+            isRunning = false;
+            // isComplete = false;
+        }
+
+        public void Cancel()
+        {
+            isRunning = false;
+            // isComplete = false;
+        }
+    }
+
+    public class Targeting
+    {
+        public string name;
+
+        public TargetingParameters parameters;
+
+        public List<Target> targets = new ();
+    }
+    
+    public struct AbilitiesComponent : IEntityComponent
+    {
+        public List<Ability> abilities;
+        public List<Targeting> targetings;
+
+        public Ability GetAbility(string name)
+        {
+            return abilities.FirstOrDefault(a => a.name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+        }
+        
+        public Targeting GetTargeting(string name)
+        {
+            return targetings.FirstOrDefault(a => a.name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+    
+    public struct ProjectileComponent : IEntityComponent
+    {
+        public Vector2 startPosition;
+        public Vector2 startDirection;
+
+        public bool started;
     }
 }
