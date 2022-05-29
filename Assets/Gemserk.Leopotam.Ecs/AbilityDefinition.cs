edbc50e8b1d8af718d45d8b119788add2da318cd
Ariel Coppes <ariel.coppes@gmail.com>
2022-05-28 23:24:58 -0300

    moved ability def to core and separated from targeting

---
+++ Assets/Gemserk.Leopotam.Ecs/AbilityDefinition.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace Gemserk.Leopotam.Ecs
+{
+    public class AbilityDefinition : MonoBehaviour, IEntityDefinition
+    {
+        public float cooldown;
+        public float duration;
+
+        public GameObject projectileDefinitionPrefab;
+
+        public Ability.StartType startType;
+
+        public void Apply(World world, int entity)
+        {
+            ref var abilitiesComponent = ref world.GetComponent<AbilitiesComponent>(entity);
+        
+            IEntityDefinition projectileDefinition = null;
+
+            if (projectileDefinitionPrefab != null)
+            {
+                projectileDefinition = projectileDefinitionPrefab
+                    .GetComponentInChildren<IEntityDefinition>();
+            }
+            
+            abilitiesComponent.abilities.Add(new Ability
+            {
+                name = gameObject.name,
+                duration = duration,
+                cooldownTotal = cooldown,
+                projectileDefinition = projectileDefinition,
+                startType = startType
+            });
+        }
+    }
+}
