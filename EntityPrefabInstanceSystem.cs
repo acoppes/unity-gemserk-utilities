dd09c4873d3623aeace89d66ff88ed8d4ed3bfdb
Ariel Coppes <ariel.coppes@gmail.com>
2022-06-08 19:15:28 -0300

    Improve EntityPrefabInstanceSystem
    Cache all EntityPrefabInstance on system inits by exploring all loaded scenes and storing them. On update, it spawns all active objects and disable them.

--- EntityPrefabInstanceSystem.cs
+++ EntityPrefabInstanceSystem.cs
@@ -1,18 +1,41 @@
+using System.Collections.Generic;
 using Leopotam.EcsLite;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Gemserk.Leopotam.Ecs
 {
-    public class EntityPrefabInstanceSystem : BaseSystem, IEcsRunSystem
+    public class EntityPrefabInstanceSystem : BaseSystem, IEcsRunSystem, IEcsInitSystem
     {
+        private List<EntityPrefabInstance> prefabInstances = new List<EntityPrefabInstance>();
+        
+        public void Init(EcsSystems systems)
+        {
+            // prefabInstances = FindObjectsOfType<EntityPrefabInstance>();
+
+            for (var i = 0; i < SceneManager.sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                foreach (var rootGameObject in scene.GetRootGameObjects())
+                {
+                    prefabInstances.AddRange(
+                        rootGameObject.GetComponentsInChildren<EntityPrefabInstance>(true));
+                }
+            }
+            
+        }
+        
         public void Run(EcsSystems systems)
         {
             // TODO: optimize this, maybe auto register by prefab instances or maybe there is no system and 
             // just a factory method called in monobehaviour awake/start.
-            var prefabInstances = FindObjectsOfType<EntityPrefabInstance>();
+            // var prefabInstances = FindObjectsOfType<EntityPrefabInstance>();
 
             foreach (var prefabInstance in prefabInstances)
             {
+                if (!prefabInstance.isActiveAndEnabled)
+                    continue;
+                
                 var parameters = prefabInstance.GetComponentsInChildren<IEntityInstanceParameter>();
                 
                 var definitionObject = prefabInstance.entityDefinitionPrefab as GameObject;
@@ -28,5 +51,7 @@ namespace Gemserk.Leopotam.Ecs
                 prefabInstance.gameObject.SetActive(false);
             }
         }
+
+
     }
 }
