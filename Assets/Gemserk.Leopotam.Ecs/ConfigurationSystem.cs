d9db05030352d4e070e0b9390956ee26ba3aeb94
Ariel Coppes <ariel.coppes@gmail.com>
2022-06-19 22:10:28 -0300

    First version of configuration system

---
+++ Assets/Gemserk.Leopotam.Ecs/ConfigurationSystem.cs
@@ -0,0 +1,32 @@
+﻿using Leopotam.EcsLite;
+
+namespace Gemserk.Leopotam.Ecs
+{
+    public class ConfigurationSystem : BaseSystem, IEntityCreatedHandler, IEcsRunSystem
+    {
+        public void OnEntityCreated(World world, Entity entity)
+        {
+            if (world.HasComponent<ConfigurationComponent>(entity))
+            {
+                ref var configuration = ref world.GetComponent<ConfigurationComponent>(entity);
+                configuration.configuration?.Configure(world, entity);
+                configuration.configuredVersion++;
+            }
+        }
+
+        public void Run(EcsSystems systems)
+        {
+            var configurations = world.GetComponents<ConfigurationComponent>();
+            foreach (var entity in world.GetFilter<ConfigurationComponent>().End())
+            {
+                ref var configuration = ref configurations.Get(entity);
+                if (configuration.reconfigure)
+                {
+                    configuration.configuration?.Configure(world, entity);
+                    configuration.reconfigure = false;
+                    configuration.configuredVersion++;
+                }
+            }
+        }
+    }
+}
