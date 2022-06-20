b3cbb451b3be9c20e1b085080f01a7ba2def4459
Ariel Coppes <ariel.coppes@gmail.com>
2022-06-19 23:27:00 -0300

    Added controller base

---
+++ Assets/Gemserk.Leopotam.Gameplay/Controllers/ControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Gemserk.Leopotam.Ecs.Controllers
+{
+    public abstract class ControllerBase : MonoBehaviour, IController
+    {
+        protected World world;
+        protected Entity entity;
+
+        public void Bind(World world, Entity entity)
+        {
+            this.world = world;
+            this.entity = entity;
+        }
+
+        public abstract void OnUpdate(float dt);
+    }
+}
