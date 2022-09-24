d988a374e927eef6697350b9694405dc17b5bd57
Ariel Coppes <ariel.coppes@gmail.com>
2022-09-23 21:04:32 -0300

    Fixed untiy editor import for builds

--- Assets/Gemserk.Leopotam.Ecs/World.cs
+++ Assets/Gemserk.Leopotam.Ecs/World.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
+#if UNITY_EDITOR
 using Leopotam.EcsLite.UnityEditor;
+#endif
 using UnityEngine;
 
 namespace Gemserk.Leopotam.Ecs
