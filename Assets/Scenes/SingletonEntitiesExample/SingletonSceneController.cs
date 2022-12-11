38cf2b45cdcfc1fc21d4cb50c914163bbe6273ca
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-11 11:43:05 -0300

    Test case for adding component after creation

---
+++ Assets/Scenes/SingletonEntitiesExample/SingletonSceneController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SingletonSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var emptyEntity = World.Instance.CreateEntity();
+        World.Instance.AddComponent<NameComponent>(emptyEntity, new NameComponent()
+        {
+            singleton = true,
+            name = "UniqueName1"
+        });
+
+        StartCoroutine(CheckForUniqueEntity());
+    }
+
+    IEnumerator CheckForUniqueEntity()
+    {
+        yield return null;
+        var entity = World.Instance.GetEntityByName("UniqueName1");
+        Assert.IsFalse(entity == Entity.NullEntity);
+    }
+}
