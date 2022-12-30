c6485932e87ebfad3cc536dc0a3368750380fd8c
Ariel Coppes <ariel.coppes@gmail.com>
2022-12-30 16:04:44 -0300

    Adding an entity definition attribute validator

---
+++ Editor/EntityDefinitionPropertyDrawer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.Leopotam.Ecs.Editor
+{
+    [CustomPropertyDrawer(typeof(EntityDefinitionAttribute))]
+    public class EntityDefinitionPropertyDrawer: PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.ObjectField(position, property, label);
+            if (EditorGUI.EndChangeCheck())
+            {
+                var referencedObject =  property.objectReferenceValue;
+                
+                if (referencedObject is IEntityDefinition)
+                {
+                    return;
+                }
+                
+                if (referencedObject is GameObject gameObject)
+                {
+                    var isEntityDefinition = gameObject.GetComponentInChildren<IEntityDefinition>() != null;
+                    if (!isEntityDefinition)
+                    {
+                        property.objectReferenceValue = null;
+                        Debug.Log("Invalid object, not an EntityDefinition.");
+                    }
+                }
+                else
+                {
+                    property.objectReferenceValue = null;
+                    Debug.Log("Invalid object, not an EntityDefinition.");
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ EntityDefinitionAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace Gemserk.Leopotam.Ecs
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class)]
+    public class EntityDefinitionAttribute : PropertyAttribute
+    {
+        
+    }
+}
