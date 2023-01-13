0b6865a69e79037be26f6ffc71fefa0440c1b7d3
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-13 10:48:53 -0300

    Moved type validartion to gameplay package

--- EntityDefinitionAttribute.cs
+++ EntityDefinitionAttribute.cs
@@ -1,19 +1,8 @@
 using System;
-using UnityEngine;
+using Gemserk.Gameplay;
 
 namespace Gemserk.Leopotam.Ecs
 {
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class)]
-    public class TypeValidationAttribute : PropertyAttribute
-    {
-        public Type typeToValidate;
-
-        public TypeValidationAttribute(Type typeToValidate)
-        {
-            this.typeToValidate = typeToValidate;
-        }
-    }
-    
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Class)]
     public class EntityDefinitionAttribute : TypeValidationAttribute
     {
