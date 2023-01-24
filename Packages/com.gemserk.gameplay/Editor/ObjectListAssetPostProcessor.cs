cc5e363d0959f47a045538f02e54a16ceb8dee75
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-24 16:06:25 -0300

    Added object list asset

---
+++ Packages/com.gemserk.gameplay/Editor/ObjectListAssetPostProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.Gameplay.Editor
+{
+    public class ObjectListAssetPostProcessor : AssetPostprocessor
+    {
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPaths)
+        {
+            var objectListAssets = AssetDatabase.FindAssets("t:ObjectListAsset")
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<ObjectListAsset>).ToList();
+            
+            foreach (var objectListAsset in objectListAssets)
+            {
+                if (string.IsNullOrEmpty(objectListAsset.path))
+                    continue;
+
+                var shouldRegenerate = false;
+
+                foreach (var asset in importedAssets)
+                {
+                    shouldRegenerate = asset.StartsWith(objectListAsset.path); // && asset.EndsWith(".prefab");
+                }
+                
+                foreach (var asset in deletedAssets)
+                {
+                    shouldRegenerate = asset.StartsWith(objectListAsset.path); // && asset.EndsWith(".prefab");
+                }
+
+                if (shouldRegenerate)
+                {
+                    objectListAsset.assets.Clear();
+
+                    objectListAsset.assets = AssetDatabase.FindAssets("t:Object", new[]
+                        {
+                            objectListAsset.path
+                        })
+                        .Select(AssetDatabase.GUIDToAssetPath)
+                        .Select(AssetDatabase.LoadAssetAtPath<Object>).ToList();
+
+                    EditorUtility.SetDirty(objectListAsset);
+                }
+                
+                AssetDatabase.SaveAssetIfDirty(objectListAsset);    
+            }
+        }
+    }
+}
---
+++ Packages/com.gemserk.gameplay/ObjectListAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gemserk.Gameplay
+{
+    [CreateAssetMenu(menuName = "Gemserk/Object List", fileName = "ObjectListAsset", order = 0)]
+    public class ObjectListAsset : ScriptableObject
+    {
+        public string path;
+        
+        public List<Object> assets = new List<Object>();
+    }
+}
