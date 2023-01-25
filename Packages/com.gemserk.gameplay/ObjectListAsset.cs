d75066c5daa522811a174bc24d2ab8861a367a23
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-24 21:48:11 -0300

    Changed a bit API

--- Packages/com.gemserk.gameplay/ObjectListAsset.cs
+++ Packages/com.gemserk.gameplay/ObjectListAsset.cs
@@ -22,15 +22,26 @@ namespace Gemserk.Gameplay
         
         public List<Object> assets = new List<Object>();
 
-        public T GetByName<T>(string name) where T : Object
+        public T FindByName<T>(string name) where T : Object
         {
             return assets.FirstOrDefault(obj => obj.name.Equals(name, defaultComparison)) as T;
         }
         
-        public void GetByName<T>(string name, List<T> results) where T : Object
+        public void CollectByName<T>(string name, List<T> results) where T : Object
         {
-            results.AddRange(assets.Where(obj => obj.name.Equals(name, defaultComparison))
-                .Cast<T>());
+            results.AddRange(assets.OfType<T>().Where(obj => obj.name.Equals(name, defaultComparison)));
+        }
+        
+        public void Collect<T>(List<T> results) where T : Object
+        {
+            results.AddRange(assets.OfType<T>());
+        }
+        
+        public List<T> Get<T>() where T : Object
+        {
+            var results = new List<T>();
+            Collect(results);
+            return results;
         }
     }
 }
