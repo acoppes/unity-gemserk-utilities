e2d103e84a9c498d0efc95814fa35186649a5bfa
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-27 10:35:07 -0300

    World extensions to get definition from obj
    Changed object extension to use system obj

---
+++ WorldExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Gemserk.Utilities;
+
+namespace Gemserk.Leopotam.Ecs
+{
+    public static class WorldExtensions
+    {
+        public static Entity CreateEntity(this World world, Object definition)
+        {
+            return world.CreateEntity(definition.GetInterface<IEntityDefinition>());
+        }
+        
+        public static Entity CreateEntity(this World world, Object definition, 
+            IEnumerable<IEntityInstanceParameter> parameters)
+        {
+            return world.CreateEntity(definition.GetInterface<IEntityDefinition>(),  parameters);
+        }
+    }
+}
