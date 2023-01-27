ab10c4b53f96c7ec41cbbaa7bf752228e0651a4d
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-27 10:41:36 -0300

    changed back to use unity.object

--- Packages/com.gemserk.leopotamecs/WorldExtensions.cs
+++ Packages/com.gemserk.leopotamecs/WorldExtensions.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Gemserk.Utilities;
 
 namespace Gemserk.Leopotam.Ecs
 {
     public static class WorldExtensions
     {
-        public static Entity CreateEntity(this World world, Object definition)
+        public static Entity CreateEntity(this World world, UnityEngine.Object definition)
         {
             return world.CreateEntity(definition.GetInterface<IEntityDefinition>());
         }
         
-        public static Entity CreateEntity(this World world, Object definition, 
+        public static Entity CreateEntity(this World world, UnityEngine.Object definition, 
             IEnumerable<IEntityInstanceParameter> parameters)
         {
             return world.CreateEntity(definition.GetInterface<IEntityDefinition>(),  parameters);
