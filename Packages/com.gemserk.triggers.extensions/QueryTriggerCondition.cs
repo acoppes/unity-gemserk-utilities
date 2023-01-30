b48435a79f6d52bdf07eea66a6a2c09623fbd5f3
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-30 13:53:17 -0300

    more stuff to be reused

---
+++ Packages/com.gemserk.triggers.extensions/QueryTriggerCondition.cs
@@ -0,0 +1,34 @@
+﻿using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+using Gemserk.Triggers.Queries;
+using MyBox;
+
+namespace Beatemup.Triggers
+{
+    public class QueryTriggerCondition : TriggerCondition
+    {
+        [DisplayInspector]
+        public Query query;
+        
+        public override string GetObjectName()
+        {
+            if (query == null)
+            {
+                return $"MatchQuery()";
+            }
+            return $"MatchQuery({query.GetEntityQuery().ToString()})";
+        }
+        
+        public override bool Evaluate(object activator = null)
+        {
+            var world = World.Instance;
+
+            if (activator == null)
+            {
+                return false;
+            }
+
+            return query.GetEntityQuery().MatchQuery(world, (Entity) activator);
+        }
+    }
+}
