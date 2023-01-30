fb125f5be9a1a5e6cd013da65c7615e89cc35834
Ariel Coppes <ariel.coppes@gmail.com>
2023-01-30 12:06:31 -0300

    future base concept

---
+++ IFuture.cs
@@ -0,0 +1,39 @@
+﻿namespace Gemserk.Utilities
+{
+    public interface IFuture
+    {
+        public enum State
+        {
+            Processing,
+            Completed,
+            Cancelled
+        }
+
+        State GetState();
+
+        object GetValue();
+        
+        T GetValueAs<T>() where T : class;
+    }
+    
+    public class Future : IFuture
+    {
+        public object value;
+        public IFuture.State state;
+        
+        public IFuture.State GetState()
+        {
+            return state;
+        }
+
+        public object GetValue()
+        {
+            return value;
+        }
+
+        public T GetValueAs<T>() where T : class
+        {
+            return value as T;
+        }
+    }
+}
