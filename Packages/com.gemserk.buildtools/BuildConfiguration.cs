ab9344ea778e09ae21b20f8ed71e2b072ae4b7d5
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-01 19:18:27 -0300

    started working on build configurations

---
+++ Packages/com.gemserk.buildtools/BuildConfiguration.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Gemserk.BuildTools
+{
+    [CreateAssetMenu(menuName = "Gemserk/Build Configuration")]
+    public class BuildConfiguration : ScriptableObject
+    {
+        
+    }
+}
---
+++ Packages/com.gemserk.buildtools/Editor/BuildToolMethods.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.BuildTools.Editor
+{
+    [CustomEditor(typeof(BuildConfiguration), true)]
+    public class BuildConfigurationCustomEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            if (GUILayout.Button(new GUIContent("Load", null, "Load config in project settings")))
+            {
+                
+            }
+
+            if (GUILayout.Button(new GUIContent("Read current", null, "Overwrite with current project settings")))
+            {
+                // show dialog just in case
+            }
+        }
+    }
+}
