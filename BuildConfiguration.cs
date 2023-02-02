973cedcf1c91ed9b7712f129bc5399883fc2496d
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-01 22:32:03 -0300

    can switch between list of configured scenes

--- BuildConfiguration.cs
+++ BuildConfiguration.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Gemserk.BuildTools
@@ -5,6 +7,9 @@ namespace Gemserk.BuildTools
     [CreateAssetMenu(menuName = "Gemserk/Build Configuration")]
     public class BuildConfiguration : ScriptableObject
     {
-        
+        #if UNITY_EDITOR
+        // public string[] scenes;
+        public List<UnityEditor.SceneAsset> sceneAssets = new List<SceneAsset>();
+#endif
     }
 }
