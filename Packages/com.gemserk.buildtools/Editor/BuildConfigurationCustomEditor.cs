5e9603edb50dd7af4784acc8020e1d5554415290
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-01 22:32:03 -0300

    can switch between list of configured scenes

---
+++ Packages/com.gemserk.buildtools/Editor/BuildConfigurationCustomEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.BuildTools.Editor
+{
+    [CustomEditor(typeof(BuildConfiguration), true)]
+    public class BuildConfigurationCustomEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var buildConfiguration = target as BuildConfiguration;
+            
+            if (GUILayout.Button(new GUIContent("Load", null, "Load config in project settings")))
+            {
+                EditorBuildSettings.scenes =
+                    buildConfiguration.sceneAssets.Select(s => new EditorBuildSettingsScene(AssetDatabase.GetAssetPath(s), true))
+                        .ToArray();
+            }
+
+            if (GUILayout.Button(new GUIContent("Read current", null, "Overwrite with current project settings")))
+            {
+                // show confirmation dialog just in case
+                buildConfiguration.sceneAssets = EditorBuildSettings.scenes
+                    .Select(s => AssetDatabase.LoadAssetAtPath<SceneAsset>(s.path)).ToList();
+
+                EditorUtility.SetDirty(buildConfiguration);
+            }
+        }
+    }
+}
