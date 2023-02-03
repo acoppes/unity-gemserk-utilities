9ebd8232010084a6a7e6f41e217719a49d62fe60
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-03 11:11:09 -0300

    Added utilities to create empty prefab in folder
    Also utility to get current project window folder

---
+++ Editor/PrefabUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.Utilities.Editor
+{
+    public static class PrefabUtils
+    {
+        [MenuItem("Assets/Create/Empty Prefab")]
+        public static void CreateEmptyPrefabInCurrentFolder()
+        {
+            var go = new GameObject("EmptyGameObject");
+            try
+            {
+                var projectWindowPath = ProjectWindowUtils.GetProjectWindowCurrentPath();
+                PrefabUtility.SaveAsPrefabAsset(go, Path.Combine(projectWindowPath, $"{go.name}.prefab"));
+            } finally {
+                Object.DestroyImmediate(go);
+            }
+        }
+    }
+}
---
+++ Editor/ProjectWindowUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace Gemserk.Utilities.Editor
+{
+    public static class ProjectWindowUtils
+    {
+        public static string GetProjectWindowCurrentPath()
+        {
+            // TODO: check if project window is open?
+            var projectWindowUtilType = typeof(ProjectWindowUtil);
+            var getActiveFolderPath = projectWindowUtilType.GetMethod("GetActiveFolderPath", BindingFlags.Static | BindingFlags.NonPublic);
+            var obj = getActiveFolderPath.Invoke(null, new object[0]);
+            return obj.ToString();
+        }
+    }
+}
