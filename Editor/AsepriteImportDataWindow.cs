c2e9f20e51e34c8fcf8fea96d603b753ee62e605
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-03 23:21:55 -0300

    Added aseprite assets list window

---
+++ Editor/AsepriteImportDataWindow.cs
@@ -0,0 +1,16 @@
+﻿using Gemserk.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.Aseprite.Editor
+{
+    public class AsepriteImportDataWindow : AssetListBaseWindow<AsepriteImportData>
+    {
+        [MenuItem("Window/Gemserk/AsepriteImportData Window")]
+        public static void ShowWindow()
+        {
+            EditorWindow wnd = GetWindow<AsepriteImportDataWindow>();
+            wnd.titleContent = new GUIContent("AsepriteImportDatas");
+        }
+    }
+}
