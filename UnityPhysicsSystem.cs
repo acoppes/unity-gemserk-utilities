9bfdb21bbba816677037428f6ea51bddbe0d1797
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-05 14:07:08 -0300

    Added unity physics3d system

---
+++ UnityPhysicsSystem.cs
@@ -0,0 +1,18 @@
+﻿using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Gemserk.Leopotam.Ecs
+{
+    public class UnityPhysicsSystem : BaseSystem, IEcsRunSystem, IEcsInitSystem
+    {
+        public void Init(EcsSystems systems)
+        {
+            Physics.simulationMode = SimulationMode.Script;
+        }
+        
+        public void Run(EcsSystems systems)
+        {
+            Physics.Simulate(Time.deltaTime);
+        }
+    }
+}
