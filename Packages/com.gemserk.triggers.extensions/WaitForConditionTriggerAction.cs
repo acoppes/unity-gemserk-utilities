fe2d6359f7d0e56730a34df701c012f7fd35f886
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-13 10:06:47 -0300

    Added new wait for condition action

---
+++ Packages/com.gemserk.triggers.extensions/WaitForConditionTriggerAction.cs
@@ -0,0 +1,22 @@
+﻿namespace Gemserk.Triggers
+{
+    public class WaitForConditionTriggerAction : TriggerAction
+    {
+        public TriggerCondition condition;
+        
+        public override string GetObjectName()
+        {
+            return $"WaitForCondition({condition.GetObjectName()})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            if (!condition.Evaluate(activator))
+            {
+                return ITrigger.ExecutionResult.Running;
+            }
+
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
