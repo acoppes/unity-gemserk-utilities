17b2399c6be1ff38c47d160214849faf47e096a0
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-19 17:20:36 -0300

    query doesnt use serialize reference anymore

--- Packages/com.gemserk.triggers.extensions/Queries/Query.cs
+++ Packages/com.gemserk.triggers.extensions/Queries/Query.cs
@@ -1,15 +1,9 @@
-using System.Collections.Generic;
 using Gemserk.Utilities;
-using UnityEngine;
 
 namespace Gemserk.Triggers.Queries
 {
     public class Query : AutoNamedObject
     {
-        [SerializeReference]
-        [SubclassSelector]
-        public List<IQueryParameter> parameters = new();
-
         public EntityQuery GetEntityQuery() => EntityQuery.Create(GetComponents<IQueryParameter>());
 
         public override string GetObjectName()
