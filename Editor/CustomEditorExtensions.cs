ffb37bab2abdcb1a60c41bc9503fcb7d4ef44a40
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-19 21:59:56 -0300

    optional custom view for query custom editor like new component definitions

---
+++ Editor/CustomEditorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Gemserk.Utilities.Editor
+{
+    public static class CustomEditorExtensions
+    {
+        public static void DrawInspectorExcept(this SerializedObject serializedObject, string[] fieldsToSkip)
+        {
+            serializedObject.Update();
+            SerializedProperty prop = serializedObject.GetIterator();
+            if (prop.NextVisible(true))
+            {
+                do
+                {
+                    if (fieldsToSkip.Any(prop.name.Contains))
+                        continue;
+ 
+                    EditorGUILayout.PropertyField(serializedObject.FindProperty(prop.name), true);
+                }
+                while (prop.NextVisible(false));
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
