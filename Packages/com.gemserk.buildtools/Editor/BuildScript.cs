84eda2caca068cb32b1f3a258a3836c343e2147b
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-21 10:31:28 -0300

    Basic build script to share between projects

---
+++ Packages/com.gemserk.buildtools/Editor/BuildScript.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Linq;
+using Gemserk.RefactorTools.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.BuildTools.Editor
+{
+    public class BuildScript
+    {
+        private const string GemserkProjectName = "GEMSERK_PROJECT_NAME";
+        private const string GemserkGameBuildPath = "GEMSERK_GAME_BUILD_PATH";
+
+        public static void BuildWebGL()
+        {
+            Build(new BuildPlayerOptions
+            {
+                target = BuildTarget.WebGL,
+                targetGroup = BuildTargetGroup.WebGL,
+                options = BuildOptions.None
+            });
+        }
+        
+        public static void BuildWindows()
+        {
+            Build(new BuildPlayerOptions
+            {
+                target = BuildTarget.StandaloneWindows64,
+                targetGroup = BuildTargetGroup.Standalone,
+                options = BuildOptions.None
+            });
+        }
+        
+        public static void BuildLinux()
+        {
+            Build(new BuildPlayerOptions
+            {
+                target = BuildTarget.StandaloneLinux64,
+                targetGroup = BuildTargetGroup.Standalone,
+                options = BuildOptions.None
+            });
+        }
+        
+        public static void BuildMacos()
+        {
+            Build(new BuildPlayerOptions
+            {
+                target = BuildTarget.StandaloneOSX,
+                targetGroup = BuildTargetGroup.Standalone,
+                options = BuildOptions.None
+            });
+        }
+        
+        public static void Build(BuildPlayerOptions buildOptions)
+        {
+            var projectName = Environment.GetEnvironmentVariable(GemserkProjectName);
+            var buildPath = Environment.GetEnvironmentVariable(GemserkGameBuildPath);
+
+            if (string.IsNullOrEmpty(projectName))
+            {
+                Debug.Log($"BUILD FAILED: missing {GemserkProjectName}");
+                EditorApplication.Exit(1);
+            }
+            
+            if (string.IsNullOrEmpty(buildPath))
+            {
+                Debug.Log($"BUILD FAILED: missing {GemserkGameBuildPath}");
+                EditorApplication.Exit(1);
+            }
+
+            var buildConfiguration = 
+                AssetDatabaseExt.FindAssets<BuildConfiguration>().FirstOrDefault(b => 
+                    b.name.Equals(projectName, StringComparison.OrdinalIgnoreCase));
+
+            if (buildConfiguration == null)
+            {
+                Debug.Log($"BUILD FAILED: missing build configuration named {projectName}");
+                EditorApplication.Exit(1);
+            }
+            
+            buildConfiguration.Load();
+
+            buildOptions.locationPathName = buildPath;
+            buildOptions.scenes = EditorBuildSettings.scenes.Select(s => s.path).ToArray();
+
+            BuildPipeline.BuildPlayer(buildOptions);
+        }
+    }
+}
