2fc05cb85d4f39e8e91b2786e0389e76112231fe
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-22 14:56:44 -0300

    Added more component definitions

---
+++ Packages/com.gemserk.leopotamecs/PlayerComponentDefinition.cs
@@ -0,0 +1,15 @@
+﻿namespace Gemserk.Leopotam.Ecs
+{
+    public class PlayerComponentDefinition : ComponentDefinitionBase
+    {
+        public override string GetComponentName()
+        {
+            return nameof(PlayerComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new PlayerComponent());
+        }
+    }
+}
---
+++ Packages/com.gemserk.triggers.extensions/Queries/QueryableComponentDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace Gemserk.Triggers.Queries
+{
+    public class QueryableComponentDefinition : ComponentDefinitionBase
+    {
+        public override string GetComponentName()
+        {
+            return nameof(QueryableComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new QueryableComponent());
+        }
+    }
+}
