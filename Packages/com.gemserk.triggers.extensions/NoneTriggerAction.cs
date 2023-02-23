7e884347fb5e32e6d97bfa9782ac18792abfc5f0
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-23 11:47:15 -0300

    Added none trigger action

---
+++ Packages/com.gemserk.triggers.extensions/NoneTriggerAction.cs
@@ -0,0 +1,18 @@
+﻿namespace Gemserk.Triggers
+{
+    /// <summary>
+    /// This trigger action use just used to fill spaces like when performing random actions
+    /// </summary>
+    public class NoneTriggerAction : TriggerAction
+    {
+        public override string GetObjectName()
+        {
+            return "None()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
