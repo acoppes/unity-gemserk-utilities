00a50fa7e4c8a6298c3ce59ab52c0dd0fe74fd7d
Ariel Coppes <ariel.coppes@gmail.com>
2023-02-23 11:35:21 -0300

    Trigger to call instantiate to EntityPrefabInstance

---
+++ Packages/com.gemserk.triggers.extensions/InstantiateEntityPrefabInstance.cs
@@ -0,0 +1,39 @@
+﻿using Gemserk.Leopotam.Ecs;
+using Gemserk.Utilities;
+using UnityEngine;
+
+namespace Gemserk.Triggers
+{
+    public class InstantiateEntityPrefabInstance : TriggerAction
+    {
+        public Object entityInstance;
+        
+        public override string GetObjectName()
+        {
+            if (entityInstance != null)
+            {
+                return $"InstantiateEntity({entityInstance.name})";
+            }
+            return "InstantiateEntity()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            if (entityInstance == null)
+            {
+                return ITrigger.ExecutionResult.Completed;
+            }
+
+            var entityPrefabInstance = entityInstance.GetInterface<EntityPrefabInstance>();
+            
+            if (entityPrefabInstance == null)
+            {
+                return ITrigger.ExecutionResult.Completed;
+            }
+            
+            entityPrefabInstance.InstantiateEntity();
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
