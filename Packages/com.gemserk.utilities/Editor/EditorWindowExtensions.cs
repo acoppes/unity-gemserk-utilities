56fd455b62734230b75d3573573d27c013fa5055
Ariel Coppes <ariel.coppes@gmail.com>
2023-04-16 13:51:59 -0300

    New editor util to edit window script

---
+++ Packages/com.gemserk.utilities/Editor/EditorWindowExtensions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Gemserk.Utilities.Editor
+{
+    public static class EditorWindowExtensions
+    {
+        public static void OpenScriptToEdit(string scriptName)
+        {
+            var files = AssetDatabase.FindAssets($"t:Script {scriptName}");
+                
+            if (files.Length >0)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(files[0]);
+                AssetDatabase.OpenAsset(AssetDatabase.LoadAssetAtPath(path, typeof(Object)));
+            }
+        }
+        
+        public static void AddEditScript(GenericMenu genericMenu, string scriptName)
+        {
+            genericMenu.AddItem(new GUIContent("Edit Script", "Opens the script in the IDE."), false, delegate
+            {
+                OpenScriptToEdit(scriptName);
+            });
+        }
+    }
+}
