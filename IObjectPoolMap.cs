d5ccaff9329f99f0ad33f5ad5b0f5bad8df5a285
Ariel Coppes <ariel.coppes@gmail.com>
2023-04-25 10:39:38 -0300

    Adding gameobject pool map API

---
+++ IObjectPoolMap.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using Object = UnityEngine.Object;
+
+namespace Gemserk.Utilities
+{
+    public interface IObjectPoolMap<T, K> 
+        where T: class where K: class
+    {
+        T Get(K key);
+
+        PooledObject<T> Get(K key, out T v);
+
+        void Release(T element);
+
+        void Clear();
+    }
+
+    public abstract class ObjectPoolMap<T, K> : IObjectPoolMap<T, K> 
+        where T : class 
+        where K : class
+    {
+        protected IDictionary<K, IObjectPool<T>> pools = new Dictionary<K, IObjectPool<T>>();
+
+        protected abstract IObjectPool<T> GetOrCreatePool(K key);
+        
+        public T Get(K key)
+        {
+            return GetOrCreatePool(key).Get();
+        }
+
+        public PooledObject<T> Get(K key, out T v)
+        {
+            return GetOrCreatePool(key).Get(out v);
+        }
+
+        public abstract void Release(T element);
+
+        public void Clear()
+        {
+            foreach (var key in pools.Keys)
+            {
+                pools[key].Clear();
+            }
+            pools.Clear();
+        }
+    }
+
+    public class GameObjectPoolMap : ObjectPoolMap<GameObject, GameObject>
+    {
+        private const string PoolNameFormat = "~Pool-{0}";
+        
+        protected override IObjectPool<GameObject> GetOrCreatePool(GameObject key)
+        {
+            if (!pools.ContainsKey(key))
+            {
+                pools[key] = new GameObjectPool(key, string.Format(PoolNameFormat, key.name));
+            }
+            return pools[key];
+        }
+
+        public override void Release(GameObject element)
+        {
+            var poolInstance = element.GetComponent<GameObjectPool.GameObjectPoolInstance>();
+            if (poolInstance != null && poolInstance.source != null)
+            {
+                var pool = GetOrCreatePool(poolInstance.source);
+                pool.Release(element);
+            }
+            else
+            {
+                Object.Destroy(element);
+            }
+        }
+    }
+}
---
+++ Tests/GameObjectPoolTests.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Gemserk.Utilities.Tests
+{
+    public class GameObjectPoolTests
+    {
+        [Test]
+        public void PooledInstances_ShouldHave_PoolInstanceComponent()
+        {
+            var prefab = new GameObject("SuperPrefab");
+
+            var pool = new GameObjectPool(prefab, "MySuperPoolName");
+
+            var instance = pool.Get();
+            var poolInstance = instance.GetComponent<GameObjectPool.GameObjectPoolInstance>();
+            
+            Assert.IsNotNull(poolInstance);
+            Assert.AreEqual(prefab, poolInstance.source);
+        }
+        
+        [UnityTest]
+        public IEnumerator TestGameObjectPool()
+        {
+            var prefab = new GameObject("SuperPrefab");
+
+            var pool = new GameObjectPool(prefab, "MySuperPoolName");
+
+            var instance = pool.Get();
+            Assert.IsTrue(instance.activeSelf);
+            
+            Assert.AreEqual(0, pool.CountInactive);
+            
+            pool.Release(instance);
+            Assert.IsFalse(instance.activeSelf);
+            
+            Assert.AreEqual(1, pool.CountInactive);
+        
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator TestGameObjectMapPool()
+        {
+            var prefab1 = new GameObject("SuperPrefab1");
+            var prefab2 = new GameObject("SuperPrefab2");
+
+            var poolMap = new GameObjectPoolMap();
+            var instance1 = poolMap.Get(prefab1);
+            var instance2 = poolMap.Get(prefab2);
+
+            yield return null;
+            
+            poolMap.Release(instance1);
+            poolMap.Release(instance2);
+
+            Assert.IsNotNull(instance1);
+            Assert.IsNotNull(instance2);
+
+            Assert.IsFalse(instance1.activeSelf);
+            Assert.IsFalse(instance2.activeSelf);
+
+            yield return null;
+        }
+    }
+}
