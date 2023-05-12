83f16ba3a2da9ecd9328dde2399f6d585fc2c0f5
Ariel Coppes <ariel.coppes@gmail.com>
2023-05-12 14:56:10 -0300

    Extract struct for player query to use from code

--- Packages/com.gemserk.triggers.extensions/Queries/PlayerQueryParameter.cs
+++ Packages/com.gemserk.triggers.extensions/Queries/PlayerQueryParameter.cs
@@ -2,18 +2,33 @@
 
 namespace Gemserk.Triggers.Queries
 {
+    public struct PlayerEntityQuery : IQueryParameter
+    {
+        public int player;
+
+        public PlayerEntityQuery(int player)
+        {
+            this.player = player;
+        }
+        
+        public bool MatchQuery(World world, Entity entity)
+        {
+            if (!entity.Has<PlayerComponent>())
+            {
+                return false;
+            }
+            
+            return entity.Get<PlayerComponent>().player == player;
+        }
+    }
+    
     public class PlayerQueryParameter : QueryParameterBase
     {
         public int player;
         
         public override bool MatchQuery(World world, Entity entity)
         {
-            if (!world.HasComponent<PlayerComponent>(entity))
-                return false;
-
-            var playerComponent = world.GetComponent<PlayerComponent>(entity);
-
-            return playerComponent.player == player;
+            return new PlayerEntityQuery(player).MatchQuery(world, entity);
         }
 
         public override string ToString()
