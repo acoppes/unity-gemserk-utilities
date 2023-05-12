55f22df988292e9e7f44ce188d2aed5628419c31
Ariel Coppes <ariel.coppes@gmail.com>
2023-05-11 23:09:50 -0300

    Moving DestroyableComponent and system from games to core

---
+++ Packages/com.gemserk.leopotamecs/Components/DestroyableComponentDefinition.cs
@@ -0,0 +1,20 @@
+﻿namespace Gemserk.Leopotam.Ecs.Components
+{
+    public struct DestroyableComponent : IEntityComponent
+    {
+        public bool destroy;
+    }
+    
+    public class DestroyableComponentDefinition : ComponentDefinitionBase
+    {
+        public override string GetComponentName()
+        {
+            return nameof(DestroyableComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new DestroyableComponent());
+        }
+    }
+}
---
+++ Packages/com.gemserk.leopotamecs/Systems/DestroyableSystem.cs
@@ -0,0 +1,22 @@
+﻿using Gemserk.Leopotam.Ecs.Components;
+using Leopotam.EcsLite;
+
+namespace Gemserk.Leopotam.Ecs.Systems
+{
+    public class DestroyableSystem : BaseSystem, IEcsRunSystem
+    {
+        public void Run(EcsSystems systems)
+        {
+            var destroyables = world.GetComponents<DestroyableComponent>();
+            
+            foreach (var entity in world.GetFilter<DestroyableComponent>().End())
+            {
+                var destroyable = destroyables.Get(entity);
+                if (destroyable.destroy)
+                {
+                    world.DestroyEntity(world.GetEntity(entity));
+                }
+            }
+        }
+    }
+}
