708d3d49ecab5cf48ac1d804f5d3f5236a747470
Ariel Coppes <ariel.coppes@gmail.com>
2023-05-26 17:41:18 -0300

    added core triggers and destroy trigger action in extensions

---
+++ AndTriggerCondition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Gemserk.Utilities;
+
+namespace Gemserk.Triggers
+{
+    public class AndTriggerCondition : TriggerCondition
+    {
+        private readonly List<TriggerCondition> conditions = new();
+
+        private void Awake()
+        {
+            gameObject.GetComponentsInChildrenDepth1(false, true, conditions);
+        }
+
+        public override string GetObjectName()
+        {
+            return "And()";
+        }
+
+        public override bool Evaluate(object activator = null)
+        {
+            foreach (var condition in conditions)
+            {
+                if (!condition.Evaluate(activator))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
