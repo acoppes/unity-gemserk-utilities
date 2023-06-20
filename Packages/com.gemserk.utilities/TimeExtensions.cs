3bbf436ec063007096401b53bd166d5af62b0fff
Ariel Coppes <ariel.coppes@gmail.com>
2023-06-20 11:31:17 -0300

    added TimeExtensions to get current fixed frames count

---
+++ Packages/com.gemserk.utilities/TimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Gemserk.Utilities
+{
+    public static class TimeExtensions
+    {
+        public static int FixedFrameCount => Mathf.RoundToInt(Time.fixedTime / Time.fixedDeltaTime);
+    }
+}
