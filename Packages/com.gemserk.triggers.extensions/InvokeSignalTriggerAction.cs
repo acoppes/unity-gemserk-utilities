ed9afbc535cdf80f8cb927eed8536c9fa600021f
Ariel Coppes <ariel.coppes@gmail.com>
2023-07-07 10:42:52 -0300

    Added InvokeSignal trigger

---
+++ Packages/com.gemserk.triggers.extensions/InvokeSignalTriggerAction.cs
@@ -0,0 +1,25 @@
+﻿using Gemserk.Utilities.Signals;
+
+namespace Gemserk.Triggers
+{
+    [TriggerEditor("Invoke Signal")]
+    public class InvokeSignalTriggerAction : TriggerAction
+    {
+        public SignalAsset signal;
+
+        public override string GetObjectName()
+        {
+            if (signal != null)
+            {
+                return $"InvokeSignal({signal.name})";
+            }
+            return "InvokeSignal()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            signal.Signal(activator);
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
