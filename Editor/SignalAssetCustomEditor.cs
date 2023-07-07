19ae79c0df3f0ada3b940640a92bbe4770ed4c74
Ariel Coppes <ariel.coppes@gmail.com>
2023-07-07 12:33:00 -0300

    Signal button to invoke from editor

--- Editor/SignalAssetCustomEditor.cs
+++ Editor/SignalAssetCustomEditor.cs
@@ -9,23 +9,27 @@ namespace Gemserk.Utilities.Editor
     {
         public override void OnInspectorGUI()
         {
-            EditorGUI.BeginChangeCheck();
             DrawDefaultInspector();
-            
-            var defaultInspectorChanged = EditorGUI.EndChangeCheck();
 
             var signalAsset = target as SignalAsset;
 
             var handlers = signalAsset.GetHandlers();
             EditorGUILayout.BeginVertical();
             EditorGUILayout.LabelField("TOTAL", $"{handlers.Count}");
+            EditorGUI.BeginDisabledGroup(!Application.isPlaying || handlers.Count == 0);
+            if (GUILayout.Button("Signal"))
+            {
+                signalAsset.Signal(null);
+            }
+            EditorGUI.EndDisabledGroup();
+            
             EditorGUI.indentLevel++;
             foreach (var handler in handlers)
             {
                 var target = handler.Target;
                 if (target is Object unityObject)
                 {
-                    EditorGUILayout.LabelField(unityObject.name, handler.Method.Name);
+                    // EditorGUILayout.LabelField(unityObject.name, handler.Method.Name);
                     EditorGUI.BeginDisabledGroup(true);
                     EditorGUILayout.ObjectField(unityObject, typeof(Object));
                     EditorGUI.EndDisabledGroup();
