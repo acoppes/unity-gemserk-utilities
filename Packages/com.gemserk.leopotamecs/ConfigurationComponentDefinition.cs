66edc0799bea5c39ebabae399d0934faff046abc
Ariel Coppes <ariel.coppes@gmail.com>
2023-07-26 22:17:00 -0300

    moved classes to the same file

--- Packages/com.gemserk.leopotamecs/ConfigurationComponentDefinition.cs
+++ Packages/com.gemserk.leopotamecs/ConfigurationComponentDefinition.cs
@@ -3,6 +3,18 @@ using UnityEngine;
 
 namespace Gemserk.Leopotam.Ecs
 {
+    public interface IConfiguration
+    {
+        void Configure(World world, Entity entity);
+    }
+    
+    public struct ConfigurationComponent : IEntityComponent
+    {
+        public IConfiguration configuration;
+        public int configuredVersion;
+        public bool reconfigure;
+    }
+    
     public class ConfigurationComponentDefinition : ComponentDefinitionBase
     {
         public Object configuration;
