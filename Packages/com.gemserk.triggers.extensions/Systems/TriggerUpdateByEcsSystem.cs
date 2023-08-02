cd7581629c5d09ca4e387c424e07f6c0b563155b
Ariel Coppes <ariel.coppes@gmail.com>
2023-08-02 16:23:12 -0300

    System to update from ecs trigger containers using script as update type

---
+++ Packages/com.gemserk.triggers.extensions/Systems/TriggerUpdateByEcsSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Gemserk.Triggers.Systems
+{
+    public class TriggerUpdateByEcsSystem : BaseSystem, IEcsRunSystem, IEcsInitSystem
+    {
+        [NonSerialized]
+        private readonly List<TriggerSystem> triggerContainers = new ();
+
+        public void Init(EcsSystems systems)
+        {
+            triggerContainers.AddRange(FindObjectsByType<TriggerSystem>(FindObjectsInactive.Exclude, 
+                FindObjectsSortMode.None));
+        }
+        
+        public void Run(EcsSystems systems)
+        {
+            for (var i = 0; i < triggerContainers.Count; i++)
+            {
+                var triggerContainer = triggerContainers[i];
+                if (triggerContainer.updateType != TriggerSystem.UpdateType.Script)
+                    continue;
+                triggerContainer.UpdateTriggers();
+            }
+        }
+
+
+    }
+}
