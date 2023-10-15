8ad85018a95edb9857d849b9aaccc92c6e2c5bdb
Ariel Coppes <ariel.coppes@gmail.com>
2023-10-15 14:44:54 -0300

    damaged effects can show death even if no damage recieved (for example forced death)

--- Packages/com.gemserk.gamescore/Systems/DamagedEffectsSystem.cs
+++ Packages/com.gemserk.gamescore/Systems/DamagedEffectsSystem.cs
@@ -21,49 +21,50 @@ namespace Game.Systems
                 var position = attachPointsFilter.Pools.Inc3.Get(e);
                 var attachPoints = attachPointsFilter.Pools.Inc4.Get(e);
 
-                if (health.processedDamages.Count == 0)
+                if (health.processedDamages.Count > 0)
                 {
-                    continue;
+                    // TODO: only update on spawn, or maybe delegate to vfx instance
+
+                    for (var i = 0; i < damageEffects.onDamageEffects.Length; i++)
+                    {
+                        var vfxData = damageEffects.onDamageEffects[i];
+                    
+                        if (vfxData.positionType == VfxComponentData.PositionType.Ground)
+                        {
+                            vfxData.position = position.value.SetY(0);
+                        }
+                        else if (vfxData.positionType == VfxComponentData.PositionType.AttachPoint)
+                        {
+                            if (attachPoints.attachPoints.TryGetValue(vfxData.attachPoint, out var attachPoint))
+                            {
+                                vfxData.position = attachPoint.position;
+                            }
+                        }
+
+                        damageEffects.onDamageEffects[i] = vfxData;
+                    }
                 }
                 
-                // TODO: only update on spawn, or maybe delegate to vfx instance
-
-                for (var i = 0; i < damageEffects.onDamageEffects.Length; i++)
+                if (health.wasKilledLastFrame)
                 {
-                    var vfxData = damageEffects.onDamageEffects[i];
+                    for (var i = 0; i < damageEffects.onDeathEffects.Length; i++)
+                    {
+                        var vfxData = damageEffects.onDeathEffects[i];
                     
-                    if (vfxData.positionType == VfxComponentData.PositionType.Ground)
-                    {
-                        vfxData.position = position.value.SetY(0);
-                    }
-                    else if (vfxData.positionType == VfxComponentData.PositionType.AttachPoint)
-                    {
-                        if (attachPoints.attachPoints.TryGetValue(vfxData.attachPoint, out var attachPoint))
+                        if (vfxData.positionType == VfxComponentData.PositionType.Ground)
                         {
-                            vfxData.position = attachPoint.position;
+                            vfxData.position = position.value.SetY(0);
                         }
-                    }
-
-                    damageEffects.onDamageEffects[i] = vfxData;
-                }
-                
-                for (var i = 0; i < damageEffects.onDeathEffects.Length; i++)
-                {
-                    var vfxData = damageEffects.onDeathEffects[i];
-                    
-                    if (vfxData.positionType == VfxComponentData.PositionType.Ground)
-                    {
-                        vfxData.position = position.value.SetY(0);
-                    }
-                    else if (vfxData.positionType == VfxComponentData.PositionType.AttachPoint)
-                    {
-                        if (attachPoints.attachPoints.TryGetValue(vfxData.attachPoint, out var attachPoint))
+                        else if (vfxData.positionType == VfxComponentData.PositionType.AttachPoint)
                         {
-                            vfxData.position = attachPoint.position;
+                            if (attachPoints.attachPoints.TryGetValue(vfxData.attachPoint, out var attachPoint))
+                            {
+                                vfxData.position = attachPoint.position;
+                            }
                         }
-                    }
 
-                    damageEffects.onDeathEffects[i] = vfxData;
+                        damageEffects.onDeathEffects[i] = vfxData;
+                    }
                 }
 
                 // locate the damage effect in the proper position
@@ -76,39 +77,37 @@ namespace Game.Systems
                 var damageEffects = filter.Pools.Inc1.Get(e);
                 var health = filter.Pools.Inc2.Get(e);
 
-                if (health.processedDamages.Count == 0)
-                {
-                    continue;
-                }
-
                 var entity = world.GetEntity(e);
                 
-                // it spawns a generic damage effect given a damage
-                // in the future could check for damage type
-                
-                for (var i = 0; i < damageEffects.onDamageEffects.Length; i++)
+                if (health.processedDamages.Count > 0)
                 {
-                    var vfxData = damageEffects.onDamageEffects[i];
-
-                    var offset = Vector3.zero;
-
-                    if (vfxData.randomOffsetType == VfxComponentData.RandomOffsetType.PlaneXZ)
+                    // it spawns a generic damage effect given a damage
+                    // in the future could check for damage type
+                
+                    for (var i = 0; i < damageEffects.onDamageEffects.Length; i++)
                     {
-                        var random = UnityEngine.Random.insideUnitCircle * vfxData.range;
-                        offset = new Vector3(random.x, 0, random.y);
-                    }
+                        var vfxData = damageEffects.onDamageEffects[i];
+
+                        var offset = Vector3.zero;
+
+                        if (vfxData.randomOffsetType == VfxComponentData.RandomOffsetType.PlaneXZ)
+                        {
+                            var random = UnityEngine.Random.insideUnitCircle * vfxData.range;
+                            offset = new Vector3(random.x, 0, random.y);
+                        }
                     
-                    var vfxEntity = world.CreateEntity(vfxData.definition);
-                    vfxEntity.Get<PositionComponent>().value = vfxData.position + offset;
+                        var vfxEntity = world.CreateEntity(vfxData.definition);
+                        vfxEntity.Get<PositionComponent>().value = vfxData.position + offset;
 
-                    if (entity.Has<PlayerComponent>() && vfxEntity.Has<PlayerComponent>())
-                    {
-                        vfxEntity.Get<PlayerComponent>().player = entity.Get<PlayerComponent>().player;
+                        if (entity.Has<PlayerComponent>() && vfxEntity.Has<PlayerComponent>())
+                        {
+                            vfxEntity.Get<PlayerComponent>().player = entity.Get<PlayerComponent>().player;
+                        }
                     }
                 }
                 
-                if (health.previousAliveState == HealthComponent.AliveType.Alive &&
-                    health.aliveType == HealthComponent.AliveType.Death)
+                // in the case of changing alive state, dont require to jave processed damages to consider effect
+                if (health.wasKilledLastFrame)
                 {
                     for (var i = 0; i < damageEffects.onDeathEffects.Length; i++)
                     {
