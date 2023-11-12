a9f1362ae40a3f6cb01469ed5af278a50290068c
Ariel Coppes <ariel.coppes@gmail.com>
2023-11-12 00:21:44 -0300

    offscreen component definition

--- Packages/com.gemserk.gamescore/Components/Components.cs
+++ Packages/com.gemserk.gamescore/Components/Components.cs
@@ -106,30 +106,6 @@ namespace Game.Components
         }
     }
 
-    // used to mark if entities should be disabled by being offscreen or not
-    public struct OffScreenDisableComponent : IEntityComponent
-    {
-        public enum DisableType
-        {
-            Always = 0,
-            FirstTimeOnly = 1
-        }
-        
-        public enum BoundsType
-        {
-            Fixed = 0,
-            // CopyFromModel = 1
-        }
-        
-        public BoundsType boundsType;
-        public Bounds bounds;
-        
-        // public disabletype => Everyhing, OnlyModel, Nothing
-
-        public DisableType disableType;
-        // public int disableCount;
-    }
-
     public struct ModelComponent : IEntityComponent
     {
         public enum Visiblity
---
+++ Packages/com.gemserk.gamescore/Components/OffScreenDisableComponentDefinition.cs
@@ -0,0 +1,42 @@
+﻿using Gemserk.Leopotam.Ecs;
+using UnityEngine;
+
+namespace Game.Components
+{
+    // used to mark if entities should be disabled by being offscreen or not
+    public struct OffScreenDisableComponent : IEntityComponent
+    {
+        public enum DisableType
+        {
+            Always = 0,
+            FirstTimeOnly = 1
+        }
+        
+        public enum BoundsType
+        {
+            Fixed = 0,
+            // CopyFromModel = 1
+        }
+        
+        public BoundsType boundsType;
+        public Bounds bounds;
+        
+        // public disabletype => Everyhing, OnlyModel, Nothing
+
+        public DisableType disableType;
+        // public int disableCount;
+    }
+    
+    public class OffScreenDisableComponentDefinition : ComponentDefinitionBase
+    {
+        public override string GetComponentName()
+        {
+            return nameof(OffScreenDisableComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new OffScreenDisableComponent());
+        }
+    }
+}
