c42be5899e5682fbd3da3c489c754bf349db9ea2
Ariel Coppes <ariel.coppes@gmail.com>
2023-11-19 14:56:46 -0300

    camera shake using impulses

---
+++ Packages/com.gemserk.gamescore/Components/CameraImpulseComponentDefinition.cs
@@ -0,0 +1,31 @@
+﻿using Gemserk.Leopotam.Ecs;
+using Unity.Cinemachine;
+
+namespace Game.Components
+{
+    public struct CameraImpulseComponent : IEntityComponent
+    {
+        public float force;
+        public CinemachineImpulseSource impulseSource;
+    }
+    
+    public class CameraImpulseComponentDefinition : ComponentDefinitionBase
+    {
+        public float force;
+        public CinemachineImpulseSource impulseSource;
+        
+        public override string GetComponentName()
+        {
+            return nameof(CameraImpulseComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new CameraImpulseComponent()
+            {
+                force = force,
+                impulseSource = impulseSource
+            });
+        }
+    }
+}
---
+++ Packages/com.gemserk.gamescore/Systems/CameraShakeFromImpulseSystem.cs
@@ -0,0 +1,31 @@
+﻿using Game.Components;
+using Game.Definitions;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    public class CameraShakeFromImpulseSystem : BaseSystem, IEcsRunSystem
+    {
+        readonly EcsFilterInject<Inc<CameraImpulseComponent>, Exc<DisabledComponent>> filter = default;
+        readonly EcsFilterInject<Inc<CameraImpulseComponent, DestroyableComponent>, Exc<DisabledComponent>> destroyables = default;
+
+        public void Run(EcsSystems systems)
+        {
+            foreach (var e in filter.Value)
+            {
+                ref var cameraImpulse = ref filter.Pools.Inc1.Get(e);
+                cameraImpulse.impulseSource.GenerateImpulse(cameraImpulse.force);
+            }
+            
+            foreach (var e in destroyables.Value)
+            {
+                ref var destroyable = ref destroyables.Pools.Inc2.Get(e);
+                destroyable.destroy = true;
+            }
+        }
+    }
+}
