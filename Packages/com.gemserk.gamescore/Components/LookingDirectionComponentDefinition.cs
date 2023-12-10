75f20e11af63b73a8d6824d7fc8f037128591e85
Ariel Coppes <ariel.coppes@gmail.com>
2023-12-09 22:40:04 -0300

    optional random starting angle for looking direction

--- Packages/com.gemserk.gamescore/Components/LookingDirectionComponentDefinition.cs
+++ Packages/com.gemserk.gamescore/Components/LookingDirectionComponentDefinition.cs
@@ -1,10 +1,21 @@
 ﻿using Gemserk.Leopotam.Ecs;
+using Gemserk.Utilities;
+using MyBox;
 using UnityEngine;
 
 namespace Game.Components
 {
     public class LookingDirectionComponentDefinition : ComponentDefinitionBase
     {
+        public enum StartingLookingDirectionType
+        {
+            Fixed = 0,
+            Random2d = 1
+        }
+
+        public StartingLookingDirectionType defaultLookingDirectionType = StartingLookingDirectionType.Fixed;
+        
+        [ConditionalField(nameof(defaultLookingDirectionType), false, StartingLookingDirectionType.Fixed)]
         public Vector3 defaultLookingDirection = Vector3.right;
         
         public override string GetComponentName()
@@ -14,9 +25,19 @@ namespace Game.Components
 
         public override void Apply(World world, Entity entity)
         {
+            var direction = Vector3.zero;
+
+            if (defaultLookingDirectionType == StartingLookingDirectionType.Fixed)
+            {
+                direction = defaultLookingDirection;
+            } else if (defaultLookingDirectionType == StartingLookingDirectionType.Random2d)
+            {
+                direction = RandomExtensions.RandomVector2(1, 1, 0, 360);
+            }
+            
             world.AddComponent(entity, new LookingDirection
             {
-                value = defaultLookingDirection
+                value = direction
             });
         }
     }
