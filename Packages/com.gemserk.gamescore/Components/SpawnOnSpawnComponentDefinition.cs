007dd6ef8ac120900bd7e621d7b50716fc29f92a
Ariel Coppes <ariel.coppes@gmail.com>
2024-01-06 19:44:49 -0300

    System to spawn entities together

---
+++ Packages/com.gemserk.gamescore/Components/SpawnOnSpawnComponentDefinition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Utilities;
+using UnityEngine;
+
+namespace Game.Components
+{
+    public struct SpawnOnSpawnComponent : IEntityComponent
+    {
+        public List<IEntityDefinition> definitions;
+    }
+    
+    public class SpawnOnSpawnComponentDefinition : ComponentDefinitionBase
+    {
+        public List<Object> definitions;
+        
+        public override string GetComponentName()
+        {
+            return nameof(SpawnOnSpawnComponent);
+        }
+
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new SpawnOnSpawnComponent()
+            {
+                definitions = definitions.Select(d => d.GetInterface<IEntityDefinition>()).ToList()
+            });
+        }
+    }
+}
---
+++ Packages/com.gemserk.gamescore/Systems/SpawnOnSpawnSystem.cs
@@ -0,0 +1,31 @@
+﻿using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
+using Gemserk.Utilities.Signals;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace Game.Systems
+{
+    public class SpawnOnSpawnSystem : BaseSystem, IEcsRunSystem
+    {
+        readonly EcsFilterInject<Inc<SpawnOnSpawnComponent, PositionComponent>, Exc<DisabledComponent>> filter = default;
+        
+        public void Run(EcsSystems systems)
+        {
+            foreach (var e in filter.Value)
+            {
+                var spawnOnSpawn = filter.Pools.Inc1.Get(e);
+                var position = filter.Pools.Inc2.Get(e);
+
+                foreach (var spawnDefinition in spawnOnSpawn.definitions)
+                {
+                    var spawnedEntity = world.CreateEntity(spawnDefinition);
+                    spawnedEntity.Get<PositionComponent>().value = position.value;
+                }
+               
+                world.RemoveComponent<SpawnOnSpawnComponent>(e);
+            }
+        }
+    }
+}
