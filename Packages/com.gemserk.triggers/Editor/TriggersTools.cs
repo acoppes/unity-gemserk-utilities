a434a76a33a474fbf92f0cc03fe113d6a2560295
Ariel Coppes <ariel.coppes@gmail.com>
2024-04-05 11:27:57 -0300

    Just created a utils to create the trigger system root

---
+++ Packages/com.gemserk.triggers/Editor/TriggersTools.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Gemserk.Triggers.Editor
+{
+    public static class TriggersTools
+    {
+        [UnityEditor.MenuItem("Tools/Triggers/Create Triggers Root")]
+        public static void CreateTriggersRoot()
+        {
+            var triggerSystems = GameObject.FindObjectsByType<TriggerSystem>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+            
+            // Don't create two systems.
+            if (triggerSystems.Length > 0)
+            {
+                return;
+            }
+
+            var triggerSystemObject = new GameObject("~TriggerSystem");
+            triggerSystemObject.AddComponent<TriggerSystem>();
+        }
+    }
+}
