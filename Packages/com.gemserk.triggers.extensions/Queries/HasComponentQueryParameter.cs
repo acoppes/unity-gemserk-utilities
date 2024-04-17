d27bda6746ef5e5e1d2809541f2abdd5ba825ee8
Ariel Coppes <ariel.coppes@gmail.com>
2024-04-17 14:06:37 -0300

    base has component query

---
+++ Packages/com.gemserk.triggers.extensions/Queries/HasComponentQueryParameter.cs
@@ -0,0 +1,18 @@
+﻿using Gemserk.Leopotam.Ecs;
+using UnityEngine.Assertions;
+
+namespace Gemserk.Triggers.Queries
+{
+    public abstract class HasComponentQueryParameter<T> : QueryParameterBase where T : struct, IEntityComponent
+    {
+        public override bool MatchQuery(Entity entity)
+        {
+            return entity.Has<T>();
+        }
+
+        public override string ToString()
+        {
+            return $"Has({typeof(T).Name})";
+        }
+    }
+}
