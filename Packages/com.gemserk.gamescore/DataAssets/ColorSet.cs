2a27b3ac5ba5e06a6a308abf3220175d13f7aca8
Ariel Coppes <ariel.coppes@gmail.com>
2024-06-11 14:53:42 -0300

    added color set scriptableobject

---
+++ Packages/com.gemserk.gamescore/DataAssets/ColorSet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.DataAssets
+{
+    [CreateAssetMenu(menuName="Gemserk/ColorSet")]
+    public class ColorSet : ScriptableObject
+    {
+        public Color[] colors;
+    }
+
+    public static class ColorSetExtensions
+    {
+        public static Texture2D CreateLutTexture(this ColorSet colorSet)
+        {
+            var lutColors = new Color[colorSet.colors.Length];
+                    
+            Array.Copy(colorSet.colors, lutColors, lutColors.Length);
+                    
+            var lutTexture = new Texture2D(lutColors.Length, 1, TextureFormat.ARGB32, false);
+            lutTexture.filterMode = FilterMode.Point;
+            lutTexture.wrapMode = TextureWrapMode.Clamp;
+            lutTexture.SetPixels(lutColors);
+            lutTexture.Apply();
+            
+            return lutTexture;
+        }
+    }
+}
