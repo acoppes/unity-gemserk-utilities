c337e9febf42de5fb71c5619df9f403e1d49ae8d
Ariel Coppes <ariel.coppes@gmail.com>
2024-06-14 22:58:28 -0300

    testing some audio triggers

---
+++ Packages/com.gemserk.gamescore/Triggers/AudioSourceTriggerEvent.cs
@@ -0,0 +1,46 @@
+﻿using Gemserk.Triggers;
+using UnityEngine;
+
+namespace Game.Triggers
+{
+    public class AudioSourceTriggerEvent : TriggerEvent
+    {
+        public enum EventType
+        {
+            Stop = 0,
+            Play = 1
+        }
+
+        public EventType eventType = EventType.Stop;
+        
+        public AudioSource target;
+
+        private bool wasPlaying;
+
+        private void Awake()
+        {
+            wasPlaying = target.isPlaying;
+        }
+
+        public void FixedUpdate()
+        {
+            if (eventType == EventType.Stop)
+            {
+                if (wasPlaying && !target.isPlaying)
+                {
+                    trigger.QueueExecution(target.gameObject);
+                }
+            }
+
+            if (eventType == EventType.Play)
+            {
+                if (!wasPlaying && target.isPlaying)
+                {
+                    trigger.QueueExecution(target.gameObject);
+                }
+            }
+
+            wasPlaying = target.isPlaying;
+        }
+    }
+}
--- Packages/com.gemserk.gamescore/Triggers/PlayMusicTriggerAction.cs
+++ Packages/com.gemserk.gamescore/Triggers/PlayMusicTriggerAction.cs
@@ -1,4 +1,5 @@
 ﻿using Gemserk.Triggers;
+using MyBox;
 using UnityEngine;
 
 namespace Game.Triggers
@@ -8,14 +9,20 @@ namespace Game.Triggers
         public enum ActionType
         {
             Play = 0,
-            Stop = 1
+            Stop = 1, 
+            Seek = 2,
+            SeekAlpha = 3
         }
         
         [SerializeField]
         private AudioSource musicSource;
 
         public ActionType actionType;
+        [ConditionalField(nameof(actionType), false, ActionType.Play)]
         public bool loop;
+
+        [ConditionalField(nameof(actionType), false, ActionType.Play, ActionType.Seek, ActionType.SeekAlpha)]
+        public float time;
 
         public override string GetObjectName()
         {
@@ -25,18 +32,48 @@ namespace Game.Triggers
             }
             return $"Music{actionType}({loop})";
         }
+
+        private float GetSeekTime()
+        {
+            var duration = musicSource.clip.length;
+            var seekTime = time;
+
+            if (seekTime > duration)
+            {
+                seekTime = duration;
+            }
+
+            if (time < 0)
+            {
+                seekTime = duration + time;
+            }
+
+            return seekTime;
+        }
         
         public override ITrigger.ExecutionResult Execute(object activator = null)
         {
             if (actionType == ActionType.Play)
             {
                 musicSource.loop = loop;
+                musicSource.time = GetSeekTime();
                 musicSource.Play();
             }
             
             if (actionType == ActionType.Stop)
             {
                 musicSource.Stop();
+            }  
+            
+            if (actionType == ActionType.Seek)
+            {
+                musicSource.time = GetSeekTime();
+            }  
+            
+            if (actionType == ActionType.SeekAlpha)
+            {
+                var duration = musicSource.clip.length;
+                musicSource.time = Mathf.Lerp(0, duration, time);
             }  
 
             return ITrigger.ExecutionResult.Completed;
