47be92001178e29afaee41fdb1462bfbd0edcdc1
Ariel Coppes <ariel.coppes@gmail.com>
2024-06-23 15:37:41 -0300

    new menu option to create build with open scenes

--- Packages/com.gemserk.buildtools/Editor/BuildConfigurationsWindow.cs
+++ Packages/com.gemserk.buildtools/Editor/BuildConfigurationsWindow.cs
@@ -1,6 +1,15 @@
-﻿using Gemserk.Utilities.Editor;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Gemserk.Utilities.Editor;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
 
 namespace Gemserk.BuildTools.Editor
 {
@@ -17,6 +26,81 @@ namespace Gemserk.BuildTools.Editor
             // This method is called when the user selects the menu item in the Editor
             EditorWindow wnd = GetWindow<BuildConfigurationsWindow>();
             wnd.titleContent = new GUIContent("Build Configurations");
+        }
+
+        [MenuItem("Gemserk/Builds/Build Open Scenes")]
+        public static void BuildCurrentScenes()
+        {
+            if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                return;
+            }
+            
+            var buildScenes = new List<string>();
+            var mainScene = "";
+            
+            for (var i = 0; i < SceneManager.sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                buildScenes.Add(scene.path);
+
+                if (i == 0)
+                {
+                    mainScene = scene.name;
+                }
+            }
+
+            Build(new BuildPlayerOptions
+            {
+                target = BuildTarget.StandaloneWindows64,
+                targetGroup = BuildTargetGroup.Standalone,
+                options = BuildOptions.AutoRunPlayer,
+            },Path.Combine($"builds/windows/dev/{mainScene}/", $"{mainScene}.exe"), buildScenes.ToArray());
+        }
+        
+        public static void Build(BuildPlayerOptions buildOptions, string path, string[] scenes)
+        {
+            buildOptions.locationPathName = path;
+            buildOptions.scenes = scenes;
+
+            var timerBuild = Stopwatch.StartNew();
+            
+            var buildReport = BuildPipeline.BuildPlayer(buildOptions);
+
+            StringBuilder buildSummary = new StringBuilder();
+            buildSummary.AppendLine("BUILDSUMMARY");
+            buildSummary.AppendFormat("Build: {0}\n", timerBuild.Elapsed.TotalSeconds);
+
+            buildSummary.AppendLine("-----------");
+            buildSummary.AppendLine("BUILD REPORT");
+            buildSummary.AppendLine("-----------");
+            foreach (var buildReportStep in buildReport.steps)
+            {
+                buildSummary.AppendFormat("Step: {0} - Duration: {1}\n", buildReportStep.name,
+                    buildReportStep.duration.TotalSeconds);
+            }
+
+            var executableName = Path.GetFileNameWithoutExtension(path);
+            Debug.LogFormat($"BUILD-{executableName}\n{buildSummary}" );
+
+            if (buildReport.summary.result == BuildResult.Failed)
+            {
+                StringBuilder errorReport = new StringBuilder();
+                foreach (var step in buildReport.steps)
+                {
+                    foreach (var message in step.messages)
+                    {
+                        if (message.type == LogType.Error || message.type == LogType.Exception || message.type == LogType.Assert)
+                        {
+                            errorReport.AppendLine($"ERROR: step:{step.name} - {message.content}");
+                        }
+                    }
+                }
+
+                var errorMessage = $"Build Failed with :{buildReport.summary.totalErrors} errors";
+                Debug.Log($"{errorMessage}\n{errorReport.ToString()}");
+                throw new Exception(errorMessage);
+            }
         }
     }
 }
