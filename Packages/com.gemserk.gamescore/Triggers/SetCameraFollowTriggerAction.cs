585d6c478da92e435adbab1e0a714bf43ffc6044
Ariel Coppes <ariel.coppes@gmail.com>
2024-06-23 15:37:50 -0300

    option to follow gameobject

--- Packages/com.gemserk.gamescore/Triggers/SetCameraFollowTriggerAction.cs
+++ Packages/com.gemserk.gamescore/Triggers/SetCameraFollowTriggerAction.cs
@@ -17,6 +17,8 @@ namespace Game.Triggers
         public ActionType actionType;
         
         public Query query;
+
+        public GameObject targetGameObject;
         
         public string cameraName;
 
@@ -39,28 +41,37 @@ namespace Game.Triggers
             
             if (actionType == ActionType.Follow)
             {
-                var entities = world.GetEntities(query.GetEntityQuery());
-                
-                foreach (var entity in entities)
+                if (targetGameObject != null)
                 {
-                    var modelComponent = world.GetComponent<ModelComponent>(entity);
+                    virtualCamera.Follow = targetGameObject.transform;
+                    virtualCamera.LookAt = targetGameObject.transform;
+                }
+                else
+                {
+                    var entities = world.GetEntities(query.GetEntityQuery());
+                
+                    foreach (var entity in entities)
+                    {
+                        var modelComponent = world.GetComponent<ModelComponent>(entity);
                     
-                    if (!useModel)
-                    {
-                        virtualCamera.Follow = modelComponent.instance.transform;
-                        virtualCamera.LookAt = modelComponent.instance.transform;
-                    }
-                    else
-                    {
-                        virtualCamera.Follow = modelComponent.instance.model;
-                        virtualCamera.LookAt = modelComponent.instance.model;
-                    }
+                        if (!useModel)
+                        {
+                            virtualCamera.Follow = modelComponent.instance.transform;
+                            virtualCamera.LookAt = modelComponent.instance.transform;
+                        }
+                        else
+                        {
+                            virtualCamera.Follow = modelComponent.instance.model;
+                            virtualCamera.LookAt = modelComponent.instance.model;
+                        }
 
-                    if (forceCameraPosition)
-                    {
-                        virtualCamera.ForceCameraPosition(virtualCamera.Follow.transform.position, Quaternion.identity);
+                        if (forceCameraPosition)
+                        {
+                            virtualCamera.ForceCameraPosition(virtualCamera.Follow.transform.position, Quaternion.identity);
+                        }
                     }
                 }
+                
             } else if (actionType == ActionType.StopFollowing)
             {
                 virtualCamera.Follow = null;
