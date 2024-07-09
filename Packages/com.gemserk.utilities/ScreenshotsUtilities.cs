3b7b0f5eb09c3f905f007e058f9e140da08a2e2b
Ariel Coppes <ariel.coppes@gmail.com>
2024-07-09 19:28:57 -0300

    added screenshots utilities

---
+++ Packages/com.gemserk.utilities/ScreenshotsUtilities.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Gemserk.Utilities
+{
+    public static class ScreenshotsUtilities
+    {
+        private const string ScreenshotsRootFolder = "screenshots";
+        
+        public static string GenerateScreenshotName(int width, int height)
+        {
+            var date = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            return $"screenshot_{width}x{height}_{date}.png";
+        }
+        
+        public static void TakeScreenshot()
+        {
+            var rootFolder = Path.Combine(Application.dataPath, "..", ScreenshotsRootFolder);
+        
+            if (!Directory.Exists(rootFolder))
+            {
+                Directory.CreateDirectory(rootFolder);
+            }
+            
+            var screenshotName = GenerateScreenshotName(Screen.width, Screen.height);
+            ScreenCapture.CaptureScreenshot(Path.Combine(rootFolder, screenshotName));
+        }        
+    }
+}
