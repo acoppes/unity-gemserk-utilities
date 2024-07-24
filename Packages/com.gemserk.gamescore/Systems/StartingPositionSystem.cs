19b202b5c77c086159101fecb00818a367d0147f
Ariel Coppes <ariel.coppes@gmail.com>
2024-07-24 14:03:58 -0300

    startting position system considers gameobjectcomponent too now

--- Packages/com.gemserk.gamescore/Systems/StartingPositionSystem.cs
+++ Packages/com.gemserk.gamescore/Systems/StartingPositionSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Game.Components;
 using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
 using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
 
@@ -7,12 +8,6 @@ namespace Game.Systems
 {
     public class StartingPositionSystem : BaseSystem, IEntityCreatedHandler
     {
-        // readonly EcsFilterInject<Inc<PositionComponent, StartingPositionComponent>, 
-        //     Exc<DisabledComponent, Physics2dComponent, PhysicsComponent>> filter = default;
-        //
-        // readonly EcsFilterInject<Inc<Physics2dComponent, StartingPositionComponent>, 
-        //     Exc<DisabledComponent>> physics2dFilter = default;
-        
         public void OnEntityCreated(World world, Entity e)
         {
             if (world.HasComponent<StartingPositionComponent>(e))
@@ -28,34 +23,14 @@ namespace Game.Systems
                     physicsComponent.body.position = startingPosition.value;
                 }
                 
+                if (e.Has<GameObjectComponent>())
+                {
+                    ref var gameObjectComponent = ref e.Get<GameObjectComponent>();
+                    gameObjectComponent.gameObject.transform.position = startingPosition.value;
+                }
+                
                 world.RemoveComponent<StartingPositionComponent>(e);
             }
         }
-        
-        // public void Run(EcsSystems systems)
-        // {
-        //     foreach (var e in filter.Value)
-        //     {
-        //         ref var position = ref filter.Pools.Inc1.Get(e);
-        //         var startingPosition = filter.Pools.Inc2.Get(e);
-        //
-        //         position.value = startingPosition.value;
-        //         
-        //         world.RemoveComponent<StartingPositionComponent>(e);
-        //     }
-        //     
-        //     foreach (var e in physics2dFilter.Value)
-        //     {
-        //         ref var physics2d = ref physics2dFilter.Pools.Inc1.Get(e);
-        //         var startingPosition = physics2dFilter.Pools.Inc2.Get(e);
-        //
-        //         physics2d.body.position = startingPosition.value;
-        //         
-        //         world.RemoveComponent<StartingPositionComponent>(e);
-        //     }
-        //     
-        // }
-
-
     }
 }
