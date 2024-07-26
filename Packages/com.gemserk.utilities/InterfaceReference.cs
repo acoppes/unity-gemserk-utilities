5d04984755ea97dbe7c43ea1e012afe5c17511d5
Ariel Coppes <ariel.coppes@gmail.com>
2024-07-26 14:44:51 -0300

    interface reference class

---
+++ Packages/com.gemserk.utilities/InterfaceReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gemserk.Utilities
+{
+    [Serializable]
+    public class InterfaceReference<T> where T : class
+    {
+        public UnityEngine.Object source;
+
+        public T Get()
+        {
+            return source.GetInterface<T>();
+        }
+    }
+}
