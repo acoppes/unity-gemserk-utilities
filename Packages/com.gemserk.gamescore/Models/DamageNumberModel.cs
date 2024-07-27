80e3fa00b1176b31ef1909166e81cbcb63cb6898
Ariel Coppes <ariel.coppes@gmail.com>
2024-07-27 16:47:36 -0300

    damage number system no delegates logic to model

---
+++ Packages/com.gemserk.gamescore/Models/DamageNumberModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Models
+{
+    public class DamageNumberModel : MonoBehaviour
+    {
+        public Text text;
+
+        public void Play(Vector3 position, float total, Action<object> callback)
+        {
+            transform.position = position;
+            
+            text.text = $"{total:0}";
+
+            LeanTween.moveY(gameObject, position.y + 2.5f, 0.35f)
+                .setFrom(position.y + 1)
+                .setEaseOutQuad()
+                .setOnCompleteParam(gameObject)
+                .setOnComplete(callback);
+        }
+    }
+}
--- Packages/com.gemserk.gamescore/Systems/DamageNumbersSystem.cs
+++ Packages/com.gemserk.gamescore/Systems/DamageNumbersSystem.cs
@@ -1,7 +1,10 @@
+using System;
 using Game.Components;
+using Game.Models;
 using Gemserk.Leopotam.Ecs;
 using Gemserk.Utilities.Pooling;
 using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +12,9 @@ namespace Game.Systems
 {
     public class DamageNumbersSystem : BaseSystem, IEcsRunSystem, IEcsInitSystem
     {
+        readonly EcsFilterInject<Inc<HealthComponent, PositionComponent>, Exc<DisabledComponent>> 
+            filter = default;
+        
         public GameObject numbersPrefab;
 
         private GameObjectPool gameObjectPool;
@@ -28,47 +34,46 @@ namespace Game.Systems
                 return;
             }
             
-            var hitPointsComponents = world.GetComponents<HealthComponent>();
-            var positionsComponents = world.GetComponents<PositionComponent>();
-            
-            foreach (var entity in world.GetFilter<HealthComponent>()
-                         .Exc<DisabledComponent>()
-                         .Inc<PositionComponent>().End())
+            foreach (var e in filter.Value)
             {
-                var hitPoints = hitPointsComponents.Get(entity);
-                var positionComponent = positionsComponents.Get(entity);
-                
-                if (hitPoints.processedDamages.Count == 0)
+                ref var health = ref filter.Pools.Inc1.Get(e);
+                ref var positionComponent = ref filter.Pools.Inc2.Get(e);
+
+                if (health.processedDamages.Count == 0)
                 {
                     continue;
                 }
 
                 var total = 0.0f;
-                
-                foreach (var hit in hitPoints.processedDamages)
+
+                for (var i = 0; i < health.processedDamages.Count; i++)
                 {
+                    var hit = health.processedDamages[i];
                     total += hit.value;
                 }
 
-                var damageNumber = gameObjectPool.Get();
-                var position = GamePerspective.ConvertFromWorld(positionComponent.value);
-                damageNumber.transform.position = position;
+                var damageNumberGameObject = gameObjectPool.Get();
+                var model = damageNumberGameObject.GetComponent<DamageNumberModel>();
                 
-                var text = damageNumber.GetComponentInChildren<Text>();
-                text.text = $"{total:0}";
-
-                LeanTween.moveY(damageNumber, position.y + 2.5f, 0.35f)
-                    .setFrom(position.y + 1)
-                    .setEaseOutQuad()
-                    .setOnCompleteParam(damageNumber)
-                    .setOnComplete(delegate(object o)
-                    {
-                        var gameObject = o as GameObject;
-                        gameObjectPool.Release(gameObject);
-                    });
+                var position = GamePerspective.ConvertFromWorld(positionComponent.value);
+                model.Play(position, total, OnNumberAnimComplete);
+                
+                // damageNumberGameObject.transform.position = position;
+                // var text = damageNumberGameObject.GetComponentInChildren<Text>();
+                // text.text = $"{total:0}";
+                //
+                // LeanTween.moveY(damageNumberGameObject, position.y + 2.5f, 0.35f)
+                //     .setFrom(position.y + 1)
+                //     .setEaseOutQuad()
+                //     .setOnCompleteParam(damageNumberGameObject)
+                //     .setOnComplete(OnNumberAnimComplete);
             }
         }
 
-
+        private void OnNumberAnimComplete(object o)
+        {
+            var gameObject = o as GameObject;
+            gameObjectPool.Release(gameObject);
+        }
     }
 }
