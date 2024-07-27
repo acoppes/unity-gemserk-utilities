1f2193676250f2b65d9dd7ac0f43605b37f58847
Ariel Coppes <ariel.coppes@gmail.com>
2024-07-27 17:17:51 -0300

    optional use animatior for damage number

--- Packages/com.gemserk.gamescore/Models/DamageNumberModel.cs
+++ Packages/com.gemserk.gamescore/Models/DamageNumberModel.cs
@@ -6,19 +6,41 @@ namespace Game.Models
 {
     public class DamageNumberModel : MonoBehaviour
     {
+        private static readonly int Enter = Animator.StringToHash("Enter");
+        
         public Text text;
+
+        public Animator animator;
+
+        public Vector3 defaultOffset = new Vector3(0, 2.5f, 0f);
+        
+        private Action<object> callback;
 
         public void Play(Vector3 position, float total, Action<object> callback)
         {
-            transform.position = position;
+            this.callback = callback;
+            
+            transform.position = position + defaultOffset;
             
             text.text = $"{total:0}";
 
-            LeanTween.moveY(gameObject, position.y + 2.5f, 0.35f)
-                .setFrom(position.y + 1)
-                .setEaseOutQuad()
-                .setOnCompleteParam(gameObject)
-                .setOnComplete(callback);
+            if (animator != null)
+            {
+                animator.SetTrigger(Enter);
+            }
+            else
+            {
+                LeanTween.moveY(gameObject, position.y, 0.35f)
+                    .setFrom(position.y + 1)
+                    .setEaseOutQuad()
+                    .setOnCompleteParam(gameObject)
+                    .setOnComplete(callback);
+            }
+        }
+
+        public void OnAnimationComplete()
+        {
+            callback?.Invoke(gameObject);
         }
     }
 }
