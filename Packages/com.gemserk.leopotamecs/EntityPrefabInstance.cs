dee21e4f8fa168e66c20abf7003eef67f497797b
Ariel Coppes <ariel.coppes@gmail.com>
2024-08-08 08:59:51 -0300

    by default show prefabs too

--- Packages/com.gemserk.leopotamecs/EntityPrefabInstance.cs
+++ Packages/com.gemserk.leopotamecs/EntityPrefabInstance.cs
@@ -6,7 +6,7 @@ namespace Gemserk.Leopotam.Ecs
     public class EntityPrefabInstance : BaseEntityPrefabInstance
     {
         [FormerlySerializedAs("entityDefinitionPrefab")] 
-        [ObjectType(typeof(IEntityDefinition), disableAssetReferences = true, filterString = "Definition")]
+        [ObjectType(typeof(IEntityDefinition), filterString = "Definition", assetReferencesOnWhenStart = false, prefabReferencesOnWhenStart = true)]
         public UnityEngine.Object entityDefinition;
         
         public override IEntityDefinition GetEntityDefinition()
