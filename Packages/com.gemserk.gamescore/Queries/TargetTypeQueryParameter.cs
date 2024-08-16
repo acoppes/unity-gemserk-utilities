f3bb33a60ff462cce6fb75f7a2ff72fbcbcd2fdc
Ariel Coppes <ariel.coppes@gmail.com>
2024-08-16 11:28:35 -0300

    added new targettype query parameter

---
+++ Packages/com.gemserk.gamescore/Queries/TargetTypeQueryParameter.cs
@@ -0,0 +1,43 @@
+﻿using Game.Components;
+using Game.Utilities;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers.Queries;
+
+namespace Game.Queries
+{
+    public readonly struct TargetTypeParameter : IQueryParameter
+    {
+        private readonly TargetType targetType;
+
+        public TargetTypeParameter(TargetType targetType)
+        {
+            this.targetType = targetType;
+        }
+        
+        public bool MatchQuery(Entity entity)
+        {
+            if (!entity.Has<TargetComponent>())
+            {
+                return false;
+            }
+
+            var entityTargetType = entity.Get<TargetComponent>().target.targetType;
+            return targetType.HasTargetFlag(entityTargetType);
+        }
+    }
+    
+    public class TargetTypeQueryParameter : QueryParameterBase
+    {
+        public TargetType targetType;
+        
+        public override bool MatchQuery(Entity entity)
+        {
+            return new TargetTypeParameter(targetType).MatchQuery(entity);
+        }
+
+        public override string ToString()
+        {
+            return $"targetType:{targetType}";
+        }
+    }
+}
