7ba5b3eecf30fbfe036562cf45aae69a83b08364
Ariel Coppes <ariel.coppes@gmail.com>
2024-08-16 09:47:42 -0300

    new entity definition attribute which is subclass of the objectype with default values (it already validates the picked object)

--- Packages/com.gemserk.leopotamecs/EntityPrefabInstance.cs
+++ Packages/com.gemserk.leopotamecs/EntityPrefabInstance.cs
@@ -1,12 +1,10 @@
 using Gemserk.Utilities;
-using UnityEngine.Serialization;
 
 namespace Gemserk.Leopotam.Ecs
 {
     public class EntityPrefabInstance : BaseEntityPrefabInstance
     {
-        [FormerlySerializedAs("entityDefinitionPrefab")] 
-        [ObjectType(typeof(IEntityDefinition), filterString = "Definition", assetReferencesOnWhenStart = false, prefabReferencesOnWhenStart = true)]
+        [EntityDefinition]
         public UnityEngine.Object entityDefinition;
         
         public override IEntityDefinition GetEntityDefinition()
--- Packages/com.gemserk.leopotamecs/IEntityDefinition.cs
+++ Packages/com.gemserk.leopotamecs/IEntityDefinition.cs
@@ -1,7 +1,20 @@
+using Gemserk.Utilities;
+
 namespace Gemserk.Leopotam.Ecs
 {
     public interface IEntityDefinition
     {
         void Apply(World world, Entity entity);
+    }
+
+    public class EntityDefinitionAttribute : ObjectTypeAttribute
+    {
+        public EntityDefinitionAttribute() : base(typeof(IEntityDefinition))
+        {
+            filterString = "_Definition";
+            disableAssetReferences = true;
+            sceneReferencesOnWhenStart = true;
+            prefabReferencesOnWhenStart = true;
+        }
     }
 }
