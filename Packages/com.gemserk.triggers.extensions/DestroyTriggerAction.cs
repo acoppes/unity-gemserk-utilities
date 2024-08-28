7a6d6fd644aa5f90ea601be6e98e9bdee3ceb554
Ariel Coppes <ariel.coppes@gmail.com>
2024-08-28 15:26:17 -0300

    destroy trigger now supports triggertarget usage

--- Packages/com.gemserk.triggers.extensions/DestroyTriggerAction.cs
+++ Packages/com.gemserk.triggers.extensions/DestroyTriggerAction.cs
@@ -1,4 +1,7 @@
-﻿using Gemserk.Leopotam.Ecs.Components;
+﻿using System;
+using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Leopotam.Ecs.Components;
 using Gemserk.Triggers.Queries;
 using MyBox;
 
@@ -6,14 +9,32 @@ namespace Gemserk.Triggers
 {
     public class DestroyTriggerAction : WorldTriggerAction
     {
-        [DisplayInspector]
+        public enum Version
+        {
+            V0,
+            V1
+        }
+
+        public Version version = Version.V0;
+
+        [ConditionalField(nameof(version), false, Version.V0)]
         public Query query;
 
+        [ConditionalField(nameof(version), false, Version.V1)]
+        public TriggerTarget target;
+
+        public bool failIfNoTargets;
+        
         public override string GetObjectName()
         {
-            if (query != null)
+            if (version == Version.V0 && query != null)
             {
                 return $"Destroy({query})";
+            }
+            
+            if (version == Version.V1)
+            {
+                return $"Destroy({target})";
             }
 
             return "Destroy()";
@@ -21,12 +42,26 @@ namespace Gemserk.Triggers
 
         public override ITrigger.ExecutionResult Execute(object activator = null)
         {
-            var entities = world.GetEntities(query.GetEntityQuery());
+            var entities = new List<Entity>();
+            
+            if (version == Version.V0)
+            {
+                entities = world.GetEntities(query.GetEntityQuery());
+            } else if (version == Version.V1)
+            {
+                target.Get(entities, world, activator);
+            }
             
             foreach (var entity in entities)
             {
                 ref var destroyableComponent = ref world.GetComponent<DestroyableComponent>(entity);
                 destroyableComponent.destroy = true;
+            }
+
+            if (failIfNoTargets && entities.Count == 0)
+            {
+                throw new Exception("No targets found to execute trigger.");
+                // return ITrigger.ExecutionResult.Interrupt;
             }
             
             return ITrigger.ExecutionResult.Completed;
