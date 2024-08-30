fff695bde79e7eeadc510e210c8746d880c2537f
Ariel Coppes <ariel.coppes@gmail.com>
2024-08-30 10:27:04 -0300

    new level component to hold the level information for an entity

---
+++ Packages/com.gemserk.gamescore/Components/LevelComponentDefinition.cs
@@ -0,0 +1,27 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace Game.Components
+{
+    public struct LevelComponent : IEntityComponent
+    {
+        public int max;
+        public int current;
+
+        public bool IsMaxLevel => current == max - 1;
+    }
+    
+    public class LevelComponentDefinition : ComponentDefinitionBase
+    {
+        public int maxLevel;
+        public int startingLevel;
+        
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new LevelComponent
+            {
+                max = maxLevel,
+                current = startingLevel
+            });
+        }
+    }
+}
