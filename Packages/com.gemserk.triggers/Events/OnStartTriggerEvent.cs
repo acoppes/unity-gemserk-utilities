74b0eaa97c1f3b3b3b6607d718938e9f9b4106ac
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-20 23:04:39 -0300

    onstart trigger action supports both start/awake

--- Packages/com.gemserk.triggers/Events/OnStartTriggerEvent.cs
+++ Packages/com.gemserk.triggers/Events/OnStartTriggerEvent.cs
@@ -2,14 +2,35 @@ namespace Gemserk.Triggers.Events
 {
     public class OnStartTriggerEvent : TriggerEvent
     {
-        public override string GetObjectName()
+        public enum ActionType
         {
-            return "OnStart";
+            OnStart = 0,
+            OnAwake = 1
         }
 
+        public ActionType actionType = ActionType.OnStart;
+        
+        public override string GetObjectName()
+        {
+            if (actionType == ActionType.OnStart)
+                return "OnStart";
+            return "OnAwake";
+        }
+
+        private void Awake()
+        {
+            if (actionType == ActionType.OnAwake)
+            {
+                trigger.QueueExecution();
+            }
+        }
+        
         private void Start()
         {
-            trigger.QueueExecution();
+            if (actionType == ActionType.OnStart)
+            {
+                trigger.QueueExecution();
+            }
         }
     }
 }
