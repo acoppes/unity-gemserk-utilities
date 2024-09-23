2712a5fb52ea2e40be2a8209509fd44698943d9d
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-23 10:42:26 -0300

    palette autoset logic from different color sets

---
+++ Packages/com.gemserk.gamescore/Utilities/CurrentPaletteAutoSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Game.DataAssets;
+using UnityEngine;
+
+namespace Game.Utilities
+{
+    public class CurrentPaletteAutoSet : MonoBehaviour
+    {
+        public static int currentPalette = 0;
+
+        public int defaultPalette = 0;
+        
+        public GraphicPalette graphicPalette;
+        public List<ColorSet> palettes;
+
+        private void Awake()
+        {
+            currentPalette = defaultPalette;
+        }
+
+        public void Update()
+        {
+            graphicPalette.colorSet = palettes[currentPalette];
+        }
+    }
+}
