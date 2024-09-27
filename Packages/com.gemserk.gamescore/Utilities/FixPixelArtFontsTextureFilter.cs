b4097820c68bcde94d58a4111703f3dc20cbeadb
Ariel Coppes <ariel.coppes@gmail.com>
2024-09-27 11:21:12 -0300

    added a pixelart runtime fix to add in a bootstrap scene or anywhere else

---
+++ Packages/com.gemserk.gamescore/Utilities/FixPixelArtFontsTextureFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Utilities
+{
+    // to be used in bootstrap scene when the game starts to fix texture filters
+    public class FixPixelArtFontsTextureFilter : MonoBehaviour
+    {
+        public List<Font> fonts;
+
+        private void Awake()
+        {
+            foreach (var font in fonts)
+            {
+                if (font && font.material)
+                {
+                    var texture = font.material.mainTexture;
+                    if (texture)
+                    {
+                        texture.filterMode = FilterMode.Point;
+                    }
+                }
+            }
+        }
+    }
+}
