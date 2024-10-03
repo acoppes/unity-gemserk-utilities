6bb660a56daa085006b330f426c63190dbf9073e
Ariel Coppes <ariel.coppes@gmail.com>
2024-10-03 11:42:05 -0300

    added new intvalue concept to keep track of int prevoous value

---
+++ Packages/com.gemserk.utilities/IntValue.cs
@@ -0,0 +1,31 @@
+﻿namespace Gemserk.Utilities
+{
+    public struct IntValue
+    {
+        private int internalValue;
+        private int previousValue;
+
+        public int value
+        {
+            get => internalValue;
+            set
+            {
+                previousValue = internalValue;
+                internalValue = value;
+            }
+        }
+
+        public bool DidChange()
+        {
+            return previousValue != internalValue;
+        }
+        
+        public static implicit operator int(IntValue intValue) => intValue.value;
+        
+        public static implicit operator IntValue(int value) => new ()
+        {
+            internalValue = value,
+            previousValue = value
+        };
+    }
+}
