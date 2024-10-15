35382db8f0a1191bf2e779979bc4a215035093be
Ariel Coppes <ariel.coppes@gmail.com>
2024-10-15 11:39:56 -0300

    added audio utilities

---
+++ Packages/com.gemserk.gamescore/AudioMixerUtilities.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public static class AudioMixerUtilities
+    {
+        public static float ConvertFromDbToNormalizedValue(float dbValue)
+        {
+            return Mathf.Exp(dbValue / 20f);
+        }
+        
+        public static float ConvertFromNormalizedValueToDb(float value)
+        {
+            return Mathf.Log(value) * 20;
+        }
+    }
+}
