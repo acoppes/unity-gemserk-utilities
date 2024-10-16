1ccb9663726a1c83bd8672a70d1b9c356cefc6f6
Ariel Coppes <ariel.coppes@gmail.com>
2024-10-16 10:22:19 -0300

    added condition and action to interact with player prefs

---
+++ Packages/com.gemserk.gamescore/Triggers/PlayerPrefTriggerAction.cs
@@ -0,0 +1,51 @@
+﻿using Gemserk.Triggers;
+using MyBox;
+using UnityEngine;
+
+namespace Game.Triggers
+{
+    public class PlayerPrefTriggerAction : TriggerAction
+    {
+        public enum ActionType
+        {
+            SetInt = 0,
+            SetFloat = 1,
+            SetString = 2,
+            Delete = 3
+        }
+
+        public ActionType actionType;
+        public string preferenceKey;
+        
+        [ConditionalField(nameof(actionType), false, ActionType.SetInt)]
+        public int intValue;
+        [ConditionalField(nameof(actionType), false, ActionType.SetFloat)]
+        public float floatValue;
+        [ConditionalField(nameof(actionType), false, ActionType.SetString)]
+        public string stringValue;
+        
+        public override string GetObjectName()
+        {
+            return $"{actionType}({preferenceKey})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            if (actionType == ActionType.SetInt)
+            {
+                PlayerPrefs.SetInt(preferenceKey, intValue);
+            } else if (actionType == ActionType.SetFloat)
+            {
+                PlayerPrefs.SetFloat(preferenceKey, floatValue);
+            } else if (actionType == ActionType.SetString)
+            {
+                PlayerPrefs.SetString(preferenceKey, stringValue);
+            } else if (actionType == ActionType.Delete)
+            {
+                PlayerPrefs.DeleteKey(preferenceKey);
+            }
+
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
---
+++ Packages/com.gemserk.gamescore/Triggers/PlayerPrefTriggerCondition.cs
@@ -0,0 +1,31 @@
+﻿using Gemserk.Triggers;
+using UnityEngine;
+
+namespace Game.Triggers
+{
+    public class PlayerPrefTriggerCondition : TriggerCondition
+    {
+        public enum ConditionType
+        {
+            HasPref = 0
+        }
+
+        public ConditionType conditionType;
+        public string preferenceKey;
+        
+        public override string GetObjectName()
+        {
+            return $"{conditionType}({preferenceKey})";
+        }
+
+        public override bool Evaluate(object activator = null)
+        {
+            if (conditionType == ConditionType.HasPref)
+            {
+                return PlayerPrefs.HasKey(preferenceKey);
+            }
+
+            return false;
+        }
+    }
+}
