e353ce4ea6cfed4cfece0eed35d3575dca9c2fa5
Ariel Coppes <ariel.coppes@gmail.com>
2024-11-12 11:42:44 -0300

    added trigger to use gamesceneloader from triggers

---
+++ Packages/com.gemserk.gamescore/Triggers/GameSceneLoaderTriggerAction.cs
@@ -0,0 +1,21 @@
+﻿using Game.Scenes;
+using Gemserk.Triggers;
+
+namespace Game.Triggers
+{
+    public class GameSceneLoaderTriggerAction : TriggerAction
+    {
+        public string sceneName;
+        
+        public override string GetObjectName()
+        {
+            return $"GameSceneLoad({sceneName})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            GameSceneLoader.LoadNextScene(sceneName);
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
