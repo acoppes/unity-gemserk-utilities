c35124925b2c153e6933df587fc6b6a8a441c211
Ariel Coppes <ariel.coppes@gmail.com>
2024-11-18 12:01:06 -0300

    script to autoconfigure camera in canvas

---
+++ Packages/com.gemserk.gamescore/Utilities/ConfigureCanvasCamera.cs
@@ -0,0 +1,31 @@
+﻿using MyBox;
+using UnityEngine;
+
+namespace Game.Utilities
+{
+    public class ConfigureCanvasCamera : MonoBehaviour
+    {
+        public enum FindType
+        {
+            Tag = 0,
+            // Name = 1
+        }
+        
+        public Canvas canvas;
+
+        public FindType findType = FindType.Tag;
+        
+        [ConditionalField(nameof(findType), false, FindType.Tag)]
+        [Tag]
+        public string cameraTag;
+        
+        public void Start()
+        {
+            var cameraObject = GameObject.FindWithTag(cameraTag);
+            if (cameraObject != null)
+            {
+                canvas.worldCamera = cameraObject.GetComponent<Camera>();
+            }
+        }
+    }
+}
