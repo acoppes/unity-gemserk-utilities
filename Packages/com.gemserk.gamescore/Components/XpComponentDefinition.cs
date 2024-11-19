c4f034f26ee99fc65269b754fecce10ef8f26e4a
Ariel Coppes <ariel.coppes@gmail.com>
2024-11-19 11:47:01 -0300

    experience holder component and levelup by experience system

---
+++ Packages/com.gemserk.gamescore/Components/XpComponentDefinition.cs
@@ -0,0 +1,33 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace Game.Components
+{
+    public struct XpComponent : IEntityComponent
+    {
+        public float pending;
+        
+        public float current;
+        
+        public float baseTotal;
+
+        public float total;
+        public float totalIncrementPerLevel;
+    }
+    
+    public class XpComponentDefinition : ComponentDefinitionBase
+    {
+        public float total;
+        public float totalIncrementPerLevel;
+
+        public override void Apply(World world, Entity entity)
+        {
+            
+            entity.Add(new XpComponent()
+            {
+                current = 0,
+                baseTotal = total,
+                totalIncrementPerLevel = totalIncrementPerLevel
+            });
+        }
+    }
+}
---
+++ Packages/com.gemserk.gamescore/Systems/XpLevelUpSystem.cs
@@ -0,0 +1,37 @@
+﻿using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace Game.Systems
+{
+    public class XpLevelUpSystem : BaseSystem, IEcsRunSystem
+    {
+        readonly EcsFilterInject<Inc<XpComponent, LevelComponent>, Exc<DisabledComponent>> filter = default;
+        
+        public void Run(EcsSystems systems)
+        {
+            foreach (var entity in filter.Value)
+            {
+                ref var xp = ref filter.Pools.Inc1.Get(entity);
+                ref var level = ref filter.Pools.Inc2.Get(entity);
+                
+                xp.total = xp.baseTotal + xp.totalIncrementPerLevel * level.current;
+
+                xp.current += xp.pending;
+                xp.pending = 0;
+                
+                if (xp.current > xp.total)
+                {
+                    // cap
+                    xp.current = xp.total;
+                    if (!level.IsMaxLevel)
+                    {
+                        level.QueueLevelUp();
+                        xp.current = 0;
+                    }
+                }
+            }
+        }
+    }
+}
