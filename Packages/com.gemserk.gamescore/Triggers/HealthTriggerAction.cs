4fae59c5566072c01aadb0abf732d41d92d930ed
Ariel Coppes <ariel.coppes@gmail.com>
2024-11-28 11:14:54 -0300

    new trigger action to set invulnerability

---
+++ Packages/com.gemserk.gamescore/Triggers/HealthTriggerAction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+using Gemserk.Triggers.Queries;
+using MyBox;
+using UnityEngine;
+
+namespace Game.Triggers
+{
+    public class HealthTriggerAction : WorldTriggerAction
+    {
+        public enum ActionType
+        {
+            SetInvulnerable,
+            SetVulnerable
+        }
+
+        public ActionType actionType;
+        public TriggerTarget triggerTarget;
+        
+        public override string GetObjectName()
+        {
+            return $"{actionType}({triggerTarget})"; 
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = new List<Entity>();
+            
+            world.GetTriggerTargetEntities(null, triggerTarget, activator, entities);
+            
+            foreach (var entity in entities)
+            {
+                ref var health = ref world.GetComponent<HealthComponent>(entity);
+                if (actionType == ActionType.SetInvulnerable)
+                {
+                    health.invulnerableCount++;
+                }
+                else if (actionType == ActionType.SetVulnerable)
+                {
+                    health.invulnerableCount--;
+                }
+            }
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
