1b46960a05a07c633e34cb4979142cd2a130bffd
Ariel Coppes <ariel.coppes@gmail.com>
2024-12-02 09:25:04 -0300

    new script to capture mouse

---
+++ Packages/com.gemserk.gamescore/Utilities/CursorModeCapture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Utilities
+{
+    public class CursorModeCapture : MonoBehaviour
+    {
+        public bool visible;
+        public CursorLockMode lockMode = CursorLockMode.None;
+        
+        public void Start()
+        {
+            Cursor.visible = visible;
+            Cursor.lockState = lockMode;
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            if (hasFocus)
+            {
+                Cursor.visible = visible;
+                Cursor.lockState = lockMode;
+            }
+        }
+    }
+}
