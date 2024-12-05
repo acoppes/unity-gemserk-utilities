84abb95ce192d57d566dc1f08062afd6a373cc0c
Ariel Coppes <ariel.coppes@gmail.com>
2024-12-05 13:07:21 -0300

    added trigger to level up entities

---
+++ Packages/com.gemserk.gamescore/Triggers/LevelUpTriggerAction.cs
@@ -0,0 +1,30 @@
+﻿using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+using Gemserk.Triggers.Queries;
+
+namespace Game.Triggers
+{
+    public class LevelUpTriggerAction : WorldTriggerAction
+    {
+        public TriggerTarget target;
+        public int levels;
+        
+        public override string GetObjectName()
+        {
+            return $"LevelUp({levels}, {target})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = world.GetEntities(target, activator);
+
+            foreach (var entity in entities)
+            {
+                entity.Get<LevelComponent>().QueueLevelUp(levels);
+            }
+            
+            return ITrigger.ExecutionResult.Running;
+        }
+    }
+}
