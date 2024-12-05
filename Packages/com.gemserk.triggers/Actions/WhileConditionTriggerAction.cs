df94506e8227b180dd6ead3c73e5982357ae1076
Ariel Coppes <ariel.coppes@gmail.com>
2024-12-05 10:14:33 -0300

    initial version of while condition do some actions trigger, would be great for it to be easier to build in editor, and also there is a case not handled yet.

---
+++ Packages/com.gemserk.triggers/Actions/WhileConditionTriggerAction.cs
@@ -0,0 +1,51 @@
+﻿namespace Gemserk.Triggers.Actions
+{
+    public class WhileConditionTriggerAction : TriggerAction
+    {
+        public TriggerCondition condition;
+        public TriggerActionGroup actionGroup;
+
+        // private bool running;
+
+        public override string GetObjectName()
+        {
+            if (condition && actionGroup)
+            {
+                return $"While({condition.GetObjectName()}, {actionGroup.GetObjectName()})";
+            }
+            return "While()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            // if (running)
+            // {
+            //     var result =actionGroup.Execute(activator);
+            //     if (result != ITrigger.ExecutionResult.Completed)
+            //     {
+            //         return result;
+            //     }
+            // }
+            
+            if (condition.Evaluate(activator))
+            {
+                var result = actionGroup.Execute(activator);
+                
+                if (result == ITrigger.ExecutionResult.Interrupt)
+                {
+                    return result;
+                }
+
+                // if (result == ITrigger.ExecutionResult.Running)
+                // {
+                //     running = true;
+                //     return result;
+                // }
+                
+                return ITrigger.ExecutionResult.Running;
+            }
+
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
