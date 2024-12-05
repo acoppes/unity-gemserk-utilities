945fbfa3bc191ad0c69369c236a6569484fca2fd
Ariel Coppes <ariel.coppes@gmail.com>
2024-12-05 10:14:59 -0300

    initial version of execute some actions over the first element of a triggertarget result

---
+++ Packages/com.gemserk.triggers.extensions/FirstTriggerAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gemserk.Triggers.Queries;
+using Gemserk.Utilities;
+
+namespace Gemserk.Triggers
+{
+    public class FirstTriggerAction : WorldTriggerAction
+    {
+        public TriggerTarget target;
+        
+        private TriggerAction action;
+        
+        public override string GetObjectName()
+        {
+            return $"First({target})";
+        }
+
+        private void Awake()
+        {
+            action = gameObject.GetComponentInChildrenDepth1<TriggerAction>(false, true);
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = world.GetEntities(target, activator);
+            return action.Execute(entities[0]);
+        }
+    }
+}
