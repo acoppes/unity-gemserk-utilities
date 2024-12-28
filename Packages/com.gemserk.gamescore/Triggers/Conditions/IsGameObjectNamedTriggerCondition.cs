b57d88a1a126a3bb72a20f581d35967cb6a2ccb9
Ariel Coppes <ariel.coppes@gmail.com>
2024-12-28 19:42:18 -0300

    added new condition to check gameobject name

---
+++ Packages/com.gemserk.gamescore/Triggers/Conditions/IsGameObjectNamedTriggerCondition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gemserk.Triggers;
+using UnityEngine;
+
+namespace Game.Triggers.Conditions
+{
+    public class IsGameObjectNamedTriggerCondition : TriggerCondition
+    {
+        public string expectedName;
+        
+        public override string GetObjectName()
+        {
+            return $"IsGameObjectNamed({expectedName})";
+        }
+
+        public override bool Evaluate(object activator = null)
+        {
+            if (activator is GameObject activatorObject)
+            {
+                if (!activatorObject)
+                {
+                    return false;
+                }
+
+                return activatorObject.name.Equals(expectedName, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return false;
+        }
+    }
+}
