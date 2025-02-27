0aed0517741862233417fab8c3089fa266ba6544
Ariel Coppes <ariel.coppes@gmail.com>
2025-02-27 16:10:28 -0300

    integrated sendsignalobject to connect unityevents and other unity things to send signals.

---
+++ Packages/com.gemserk.utilities/Signals/SendSignalObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Gemserk.Utilities.Signals
+{
+    // this class is used to connect buttons, unity events or animator to send a signal
+    public class SendSignalObject : MonoBehaviour
+    {
+        public SignalAsset signalAsset;
+
+        public void SendSignal()
+        {
+            signalAsset.Signal(null);
+        }
+
+        public void SendSignalWithObject(object source)
+        {
+            signalAsset.Signal(source);
+        }
+    }
+}
