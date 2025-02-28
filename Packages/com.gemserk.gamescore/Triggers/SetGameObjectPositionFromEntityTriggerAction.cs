2c341e9cdf5828dff432a09e092900cdc4282879
Ariel Coppes <ariel.coppes@gmail.com>
2025-02-28 10:52:21 -0300

    added new trigger to set gameobject position from entity position

---
+++ Packages/com.gemserk.gamescore/Triggers/SetGameObjectPositionFromEntityTriggerAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+using UnityEngine;
+
+namespace Game.Triggers
+{
+    public class SetGameObjectPositionFromEntityTriggerAction : WorldTriggerAction
+    {
+        public GameObject targetGameObject;
+        public TriggerTarget positionTarget;
+
+        public override string GetObjectName()
+        {
+            if (targetGameObject)
+                return $"SetPosition({targetGameObject.name}, {positionTarget})";
+            return $"SetPosition({positionTarget})";
+        }
+        
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = new List<Entity>();
+
+            positionTarget.Get(entities, world, activator);
+            
+            foreach (var entity in entities)
+            {
+                targetGameObject.transform.position = entity.Get<PositionComponent>().value;
+            }
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
