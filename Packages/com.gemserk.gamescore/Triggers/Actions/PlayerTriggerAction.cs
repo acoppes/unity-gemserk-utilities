668a6948f5c453bfd0a4a7226dd5b574d980d1f5
Ariel Coppes <ariel.coppes@gmail.com>
2025-03-03 13:53:15 -0300

    added new trigger action to switch player team

---
+++ Packages/com.gemserk.gamescore/Triggers/Actions/PlayerTriggerAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+
+namespace Game.Triggers.Actions
+{
+    public class PlayerTriggerAction : WorldTriggerAction
+    {
+        public enum ActionType
+        {
+            SwitchTeam = 0
+        }
+        
+        public TriggerTarget target;
+
+        public ActionType actionType = ActionType.SwitchTeam;
+
+        public int newPlayerTeam;
+
+        public override string GetObjectName()
+        {
+            return $"Player{actionType}({target})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = new List<Entity>();
+            target.Get(entities, world, activator);
+            
+            foreach (var entity in entities)
+            {
+                ref var player = ref world.GetComponent<PlayerComponent>(entity);
+                player.player = newPlayerTeam;
+            }
+
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
