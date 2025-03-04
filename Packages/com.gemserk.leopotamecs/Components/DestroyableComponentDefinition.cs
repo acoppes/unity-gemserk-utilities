249a154363f02368eb4b158a2794bd99c7696304
Ariel Coppes <ariel.coppes@gmail.com>
2025-03-03 22:05:29 -0300

    feat(components): added new option to destroy after some frames by having a new delayed destroy component that disables the object as soon as it detects it was desroyed and then waits for some frames to really destroy

--- Packages/com.gemserk.leopotamecs/Components/DestroyableComponentDefinition.cs
+++ Packages/com.gemserk.leopotamecs/Components/DestroyableComponentDefinition.cs
@@ -1,11 +1,10 @@
 ﻿namespace Gemserk.Leopotam.Ecs.Components
 {
+    // instantly destroy this
     public struct DestroyableComponent : IEntityComponent
     {
         // private bool _destroy;
-
         public bool destroy;
-
         // public bool destroy
         // {
         //     get => _destroy;
@@ -19,12 +18,27 @@
         //     }
         // }
     }
+
+    // disables object and destroy in specified frames
+    public struct DelayedDestroyComponent : IEntityComponent
+    {
+        public int frames;
+    }
     
     public class DestroyableComponentDefinition : ComponentDefinitionBase
     {
+        public int framesToDestroy = 0;
+        
         public override void Apply(World world, Entity entity)
         {
             world.AddComponent(entity, new DestroyableComponent());
+            if (framesToDestroy > 0)
+            {
+                world.AddComponent(entity, new DelayedDestroyComponent()
+                {
+                    frames = framesToDestroy
+                });
+            }
         }
     }
 }
--- Packages/com.gemserk.leopotamecs/Systems/DestroyableSystem.cs
+++ Packages/com.gemserk.leopotamecs/Systems/DestroyableSystem.cs
@@ -7,16 +7,41 @@ namespace Gemserk.Leopotam.Ecs.Systems
 {
     public class DestroyableSystem : BaseSystem, IEcsRunSystem
     {
-        readonly EcsFilterInject<Inc<DestroyableComponent>> destroyableFilter = default;
+        readonly EcsFilterInject<Inc<DestroyableComponent, DelayedDestroyComponent>, Exc<DisabledComponent>> delayedDestroyableFilter = default;
+        readonly EcsFilterInject<Inc<DestroyableComponent, DelayedDestroyComponent, DisabledComponent>> disabledDelayedFilter = default;
+        readonly EcsFilterInject<Inc<DestroyableComponent>, Exc<DelayedDestroyComponent>> destroyableFilter = default;
         
         public void Run(EcsSystems systems)
         {
-            foreach (var entity in destroyableFilter.Value)
+            foreach (var e in delayedDestroyableFilter.Value)
             {
-                var destroyable = destroyableFilter.Pools.Inc1.Get(entity);
+                var destroyable = delayedDestroyableFilter.Pools.Inc1.Get(e);
                 if (destroyable.destroy)
                 {
-                    world.DestroyEntity(world.GetEntity(entity));
+                    world.AddComponent(e, new DisabledComponent());
+                }
+            }
+            
+            foreach (var e in disabledDelayedFilter.Value)
+            {
+                var destroyable = disabledDelayedFilter.Pools.Inc1.Get(e);
+                ref var delayedDestroy = ref disabledDelayedFilter.Pools.Inc2.Get(e);
+                if (destroyable.destroy)
+                {
+                    if (delayedDestroy.frames < 0)
+                    {
+                        world.DestroyEntity(world.GetEntity(e));
+                    }
+                    delayedDestroy.frames--;
+                }
+            }
+            
+            foreach (var e in destroyableFilter.Value)
+            {
+                var destroyable = destroyableFilter.Pools.Inc1.Get(e);
+                if (destroyable.destroy)
+                {
+                    world.DestroyEntity(world.GetEntity(e));
                 }
             }
         }
