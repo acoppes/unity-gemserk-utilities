71fc64048e929aaa55d5ea8b962c07717627acd0
Ariel Coppes <ariel.coppes@gmail.com>
2025-03-04 10:15:53 -0300

    gizmo model to be used just for debug

---
+++ Packages/com.gemserk.gamescore/Models/EditorGizmoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Models
+{
+    public class EditorGizmoModel : MonoBehaviour
+    {
+        public float radius;
+        
+        public bool filled;
+        public bool wire;
+        
+        private void OnDrawGizmos()
+        {
+            if (wire)
+            {
+                Gizmos.DrawWireSphere(transform.position, radius);
+            }
+            
+            if (filled)
+            {
+                Gizmos.DrawSphere(transform.position, radius);
+            }
+        }
+    }
+}
