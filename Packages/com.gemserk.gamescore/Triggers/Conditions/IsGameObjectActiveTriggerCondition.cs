18d6610d59473d90fba194b471c8d75824aa2fdf
Ariel Coppes <ariel.coppes@gmail.com>
2025-03-11 11:28:45 -0300

    added new condition to check if a gameobject is active or not

---
+++ Packages/com.gemserk.gamescore/Triggers/Conditions/IsGameObjectActiveTriggerCondition.cs
@@ -0,0 +1,34 @@
+﻿using Gemserk.Triggers;
+using UnityEngine;
+
+namespace Game.Triggers.Conditions
+{
+    public class IsGameObjectActiveTriggerCondition : TriggerCondition
+    {
+        public GameObject gameObject;
+        
+        public override string GetObjectName()
+        {
+            if (!gameObject)
+            {
+                return $"IsGameObjectActive(activator)";
+            }
+            return $"IsGameObjectActive({gameObject.name})";
+        }
+
+        public override bool Evaluate(object activator = null)
+        {
+            if (gameObject)
+            {
+                return gameObject.activeSelf;
+            }
+            
+            if (activator is GameObject activatorObject)
+            {
+                return activatorObject.activeSelf;
+            }
+
+            return false;
+        }
+    }
+}
