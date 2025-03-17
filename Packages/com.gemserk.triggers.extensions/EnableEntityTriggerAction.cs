458a849df93e53ebc9eddab9066af7fdef01b03d
Ariel Coppes <ariel.coppes@gmail.com>
2025-03-17 10:13:15 -0300

    added new version for enable entity trigger

---
+++ Packages/com.gemserk.triggers.extensions/EnableEntityTriggerAction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+
+namespace Gemserk.Triggers
+{
+    public class EnableEntityTriggerAction : WorldTriggerAction
+    {
+        public enum ActionType
+        {
+            Enable = 0,
+            Disable = 1
+        }
+
+        public ActionType actionType;
+        public TriggerTarget target;
+
+        public override string GetObjectName()
+        {
+            return $"{actionType}({target})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = new List<Entity>();
+            target.Get(entities, world, activator);
+
+            if (actionType == ActionType.Disable)
+            {
+                foreach (var entity in entities)
+                {
+                    entity.Add(new DisabledComponent());
+                }                    
+            }
+            
+            if (actionType == ActionType.Enable)
+            {
+                foreach (var entity in entities)
+                {
+                    entity.Add(new EnableDisabledComponent());
+                }                    
+            }
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
