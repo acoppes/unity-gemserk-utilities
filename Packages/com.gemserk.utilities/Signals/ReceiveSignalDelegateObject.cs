4736ae52ca6287315233b6bd3e9f37b4f593942c
Ariel Coppes <ariel.coppes@gmail.com>
2025-04-02 09:44:57 -0300

    added new feature to delegate from signal to unity event

---
+++ Packages/com.gemserk.utilities/Signals/ReceiveSignalDelegateObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Gemserk.Utilities.Signals
+{
+    public class ReceiveSignalDelegateObject : MonoBehaviour
+    {
+        public SignalAsset signalAsset;
+
+        public UnityEvent<object> unityEvent;
+
+        private void OnEnable()
+        {
+            signalAsset.Register(OnSignal);
+        }
+
+        private void OnDisable()
+        {
+            signalAsset.Unregister(OnSignal);
+        }
+
+        private void OnSignal(object userData)
+        {
+            unityEvent.Invoke(userData);
+        }
+    }
+}
