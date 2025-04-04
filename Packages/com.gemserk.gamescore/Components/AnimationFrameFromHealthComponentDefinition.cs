3e20f465a0cb0fec93351688455caddbf91fbb5d
Ariel Coppes <ariel.coppes@gmail.com>
2025-04-04 11:13:14 -0300

    added new component and system to set anim frame from health factor

---
+++ Packages/com.gemserk.gamescore/Components/AnimationFrameFromHealthComponentDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Gemserk.Leopotam.Ecs;
+
+namespace Game.Components
+{
+    public struct AnimationFromHealthComponent : IEntityComponent
+    {
+        
+    }
+    
+    public class AnimationFrameFromHealthComponentDefinition : ComponentDefinitionBase
+    {
+        public override void Apply(World world, Entity entity)
+        {
+            world.AddComponent(entity, new AnimationFromHealthComponent());
+        }
+    }
+}
---
+++ Packages/com.gemserk.gamescore/Systems/AnimationFromHealthSystem.cs
@@ -0,0 +1,28 @@
+﻿using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    public class AnimationFromHealthSystem : BaseSystem, IEcsRunSystem
+    {
+        readonly EcsFilterInject<Inc<AnimationsComponent, AnimationFromHealthComponent, HealthComponent>, Exc<DisabledComponent>> 
+            animationsFromResistance = default;
+        
+        public void Run(EcsSystems systems)
+        {
+            foreach (var e in animationsFromResistance.Value)
+            {
+                ref var animations = ref animationsFromResistance.Pools.Inc1.Get(e);
+                ref var health = ref animationsFromResistance.Pools.Inc3.Get(e);
+
+                var currentAnimation = animations.animationsAsset.animations[animations.currentAnimation];
+                
+                var currentFrameFromResistance = Mathf.RoundToInt((currentAnimation.TotalFrames - 1) * health.factor);
+                animations.currentFrame = Mathf.Clamp(currentFrameFromResistance, 0, currentAnimation.TotalFrames - 1);
+            }
+        }
+    }
+}
