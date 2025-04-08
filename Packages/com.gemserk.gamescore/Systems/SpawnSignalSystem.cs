c6af27b5b315b16efc32c215a265e0885eb8a704
Ariel Coppes <ariel.coppes@gmail.com>
2025-04-08 14:51:29 -0300

    changed old system to use filters

--- Packages/com.gemserk.gamescore/Systems/SpawnSignalSystem.cs
+++ Packages/com.gemserk.gamescore/Systems/SpawnSignalSystem.cs
@@ -2,33 +2,44 @@
 using Gemserk.Leopotam.Ecs;
 using Gemserk.Utilities.Signals;
 using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
 using UnityEngine;
 
 namespace Game.Systems
 {
     public class SpawnSignalSystem : BaseSystem, IEcsRunSystem
     {
+        readonly EcsFilterInject<Inc<SpawnSignalComponent>, Exc<DisabledComponent>> 
+            spawnSignals = default;
+        
         [SerializeField]
         private SignalAsset onSpawnedSignal;
         
         public void Run(EcsSystems systems)
         {
             // auto disable?
-            if (onSpawnedSignal == null)
+            if (!onSpawnedSignal)
             {
                 return;
             }
-            
-            foreach (var entity in world
-                         .GetFilter<SpawnSignalComponent>()
-                         .Exc<DisabledComponent>()
-                         .End())
+
+            foreach (var e in spawnSignals.Value)
             {
-                var worldEntity = world.GetEntity(entity);
-                
+                var worldEntity = world.GetEntity(e);
                 onSpawnedSignal.Signal(worldEntity);
-                world.RemoveComponent<SpawnSignalComponent>(worldEntity);
+                spawnSignals.Pools.Inc1.Del(e);
             }
+            
+            // foreach (var entity in world
+            //              .GetFilter<SpawnSignalComponent>()
+            //              .Exc<DisabledComponent>()
+            //              .End())
+            // {
+            //     var worldEntity = world.GetEntity(entity);
+            //     
+            //     onSpawnedSignal.Signal(worldEntity);
+            //     world.RemoveComponent<SpawnSignalComponent>(worldEntity);
+            // }
         }
     }
 }
