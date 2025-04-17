65aa9748818a951df112b81be92be2d9af8f79c7
Ariel Coppes <ariel.coppes@gmail.com>
2025-04-17 08:29:13 -0300

    added grids tests assembly and tests from shipminer project

---
+++ Packages/com.gemserk.datagrids/Editor/Tests/WorldGridTests.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+using UnityEngine;
+using Assert = NUnit.Framework.Assert;
+
+namespace Gemserk.DataGrids.Tests
+{
+    public class WorldGridTests
+    {
+        [Test]
+        public void WorldGrid_Convert_NonDivisibleBy2()
+        {
+            var width = 47;
+            
+            var worldGridData = new WorldGridData(new Vector2(width, 1), new Vector2(0.75f, 1));
+
+            var current = 0;
+            var p = -width * 0.5f;
+            
+            for (int i = 0; i < width; i++)
+            {
+                Assert.AreEqual(current, worldGridData.GetGridPosition(new Vector2(p, 0)).x, $"for object {i}");
+                current++;
+                p += 0.75f;
+            }
+
+        }
+        
+        [Test]
+        public void WorldGrid_Convert_BetterNumbersWorld()
+        {
+            var width = 100;
+            var worldGridData = new WorldGridData(new Vector2(width, 1), new Vector2(1, 1));
+
+            var current = 0;
+            var p = -50f;
+            
+            for (int i = 0; i < width; i++)
+            {
+                Assert.AreEqual(current, worldGridData.GetGridPosition(new Vector2(p, 0)).x, $"for object {i}");
+                current++;
+                p += 1;
+            }
+
+        }
+    }
+}
+
