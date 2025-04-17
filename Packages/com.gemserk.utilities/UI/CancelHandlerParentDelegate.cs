30589c6b075351feb92d86c1ea4b0e56b746a26a
Ariel Coppes <ariel.coppes@gmail.com>
2025-04-17 08:17:07 -0300

    moved some ui utilities from shipminer to utilities

---
+++ Packages/com.gemserk.utilities/UI/CancelHandlerParentDelegate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Gemserk.Utilities.UI
+{
+    public class CancelHandlerParentDelegate : MonoBehaviour, ICancelHandler
+    {
+        public void OnCancel(BaseEventData eventData)
+        {
+            ExecuteEvents.ExecuteHierarchy(transform.parent.gameObject, eventData, ExecuteEvents.cancelHandler);
+        }
+    }
+}
---
+++ Packages/com.gemserk.utilities/UI/SelectionHandlerDelegate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Gemserk.Utilities.UI
+{
+    public class SelectionHandlerDelegate : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        public GameObject delegateObject;
+        
+        public void OnSelect(BaseEventData eventData)
+        {
+            ExecuteEvents.Execute(delegateObject, eventData, ExecuteEvents.selectHandler);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            ExecuteEvents.Execute(delegateObject, eventData, ExecuteEvents.deselectHandler);
+        }
+    }
+}
