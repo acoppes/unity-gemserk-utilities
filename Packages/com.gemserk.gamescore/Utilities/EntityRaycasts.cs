47e427d8a4e6da5582f5158c88ce21840962d169
Ariel Coppes <ariel.coppes@gmail.com>
2025-05-08 20:09:35 -0300

    started adding some ideas to target entities using physics raycasts

---
+++ Packages/com.gemserk.gamescore/Utilities/EntityRaycasts.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using UnityEngine;
+using Vertx.Debugging;
+
+namespace Game.Utilities
+{
+    public static class EntityRaycasts
+    {
+        // TODO: make optional provide how many hits it wants...
+        
+        private static RaycastHit2D[] _tempHits = new RaycastHit2D[20];
+        
+        public static List<Entity> GetEntitiesFromPhysics2dRaycast(Vector3 position, Vector3 direction, 
+            ContactFilter2D contactFilter2D, float distance, List<Entity> entities)
+        {
+            var hitCount = DrawPhysics2D.Raycast(position, direction,
+                contactFilter2D, _tempHits, distance);
+            
+            for (var i = 0; i < hitCount; i++)
+            {
+                var hit = _tempHits[i];
+                var entityReference = hit.collider.GetComponentInParent<EntityReference>();
+
+                if (!entityReference || !entityReference.entity)
+                {
+                    continue;
+                }
+
+                // TODO: custom optional matcher
+                // if (!entityReference.entity.Has<TerrainComponent>())
+                // {
+                //     continue;
+                // }
+
+                entities.Add(entityReference.entity);
+            }
+
+            return entities;
+        }
+    }
+}
