995b1cd939553afc3a8f932a9803920bb3e30bf5
Ariel Coppes <ariel.coppes@gmail.com>
2025-05-12 12:01:55 -0300

    trying to change to get more information about entity raycast so it can be sorted later

--- Packages/com.gemserk.gamescore/Utilities/EntityRaycasts.cs
+++ Packages/com.gemserk.gamescore/Utilities/EntityRaycasts.cs
@@ -6,42 +6,26 @@ using Vertx.Debugging;
 
 namespace Game.Utilities
 {
+    public struct EntityHit
+    {
+        public Entity entity;
+        public RaycastHit2D hit;
+    }
+    
     public static class EntityRaycasts
     {
         // TODO: make optional provide how many hits it wants...
         
         private static RaycastHit2D[] _tempHits = new RaycastHit2D[20];
         
-        public static List<Entity> GetEntitiesFromPhysics2dRaycast(Vector3 position, Vector3 direction, 
-            ContactFilter2D contactFilter2D, float distance, List<Entity> entities)
+        public static List<EntityHit> GetEntitiesFromPhysics2dRaycast(Vector3 position, Vector3 direction, 
+            ContactFilter2D contactFilter2D, float distance, List<EntityHit> entities)
         {
-            var hitCount = DrawPhysics2D.Raycast(position, direction,
-                contactFilter2D, _tempHits, distance);
-            
-            for (var i = 0; i < hitCount; i++)
-            {
-                var hit = _tempHits[i];
-                var entityReference = hit.collider.GetComponentInParent<EntityReference>();
-
-                if (!entityReference || !entityReference.entity)
-                {
-                    continue;
-                }
-
-                // TODO: custom optional matcher
-                // if (!entityReference.entity.Has<TerrainComponent>())
-                // {
-                //     continue;
-                // }
-
-                entities.Add(entityReference.entity);
-            }
-
-            return entities;
+            return GetEntitiesFromPhysics2dRaycast(position, direction, contactFilter2D, distance, entities, null);
         }
         
-        public static List<Entity> GetEntitiesFromPhysics2dRaycast(Vector3 position, Vector3 direction, 
-            ContactFilter2D contactFilter2D, float distance, List<Entity> entities, Func<Entity, bool> filter)
+        public static List<EntityHit> GetEntitiesFromPhysics2dRaycast(Vector3 position, Vector3 direction, 
+            ContactFilter2D contactFilter2D, float distance, List<EntityHit> entities, Func<Entity, bool> filter)
         {
             var hitCount = DrawPhysics2D.Raycast(position, direction,
                 contactFilter2D, _tempHits, distance);
@@ -49,6 +33,7 @@ namespace Game.Utilities
             for (var i = 0; i < hitCount; i++)
             {
                 var hit = _tempHits[i];
+                
                 var entityReference = hit.collider.GetComponentInParent<EntityReference>();
 
                 if (!entityReference || !entityReference.entity)
@@ -56,12 +41,16 @@ namespace Game.Utilities
                     continue;
                 }
 
-                if (!filter(entityReference.entity))
+                if (filter != null && !filter(entityReference.entity))
                 {
                     continue;
                 }
                 
-                entities.Add(entityReference.entity);
+                entities.Add(new EntityHit()
+                {
+                    entity = entityReference.entity,
+                    hit = hit
+                });
             }
 
             return entities;
