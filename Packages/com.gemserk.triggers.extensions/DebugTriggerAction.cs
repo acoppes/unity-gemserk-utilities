39060a99ccfdf6d61979dd8e9ea3ccca0e3ec764
Ariel Coppes <ariel.coppes@gmail.com>
2025-06-13 15:01:36 -0300

    now debug trigger action can specify the logtype level added a throw exception trigger action

--- Packages/com.gemserk.triggers.extensions/DebugTriggerAction.cs
+++ Packages/com.gemserk.triggers.extensions/DebugTriggerAction.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Gemserk.Triggers
 {
@@ -6,15 +7,37 @@ namespace Gemserk.Triggers
     {
         [TextArea(1, 15)]
         public string text;
+
+        public LogType logType = LogType.Log;
         
         public override string GetObjectName()
         {
-            return "DebugLog()";
+            return $"Debug{logType}()";
         }
 
         public override ITrigger.ExecutionResult Execute(object activator = null)
         {
-            Debug.Log(string.Format(text, activator));
+            switch (logType)
+            {
+                case LogType.Error:
+                    Debug.LogError(string.Format(text, activator));
+                    break;
+                case LogType.Assert:
+                    Debug.LogAssertion(string.Format(text, activator));
+                    break;
+                case LogType.Warning:
+                    Debug.LogWarning(string.Format(text, activator));
+                    break;
+                case LogType.Log:
+                    Debug.Log(string.Format(text, activator));
+                    break;
+                case LogType.Exception:
+                    Debug.LogException(new Exception(string.Format(text, activator)));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
             return ITrigger.ExecutionResult.Completed;
         }
     }
---
+++ Packages/com.gemserk.triggers.extensions/ThrowExceptionTriggerAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gemserk.Triggers
+{
+    public class ThrowExceptionTriggerAction : TriggerAction
+    {
+        [TextArea(1, 15)]
+        public string text;
+
+        public override string GetObjectName()
+        {
+            return "ThrowExceptionWithMessage()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            throw new Exception(string.Format(text, activator));
+        }
+    }
+}
