4bc478270651997f16a5f5f9a758c509e50eeaa0
Ariel Coppes <ariel.coppes@gmail.com>
2025-06-25 14:00:22 -0300

    added a commented method that depends on the target texture and some dpi, maybe could be useful in the future

--- Packages/com.gemserk.gamescore/CameraExtensions.cs
+++ Packages/com.gemserk.gamescore/CameraExtensions.cs
@@ -13,5 +13,26 @@ namespace Game
                 new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
             return bounds;
         }
+        
+        // this one uses the target texture directly...
+        // private Bounds GetBounds2(Camera camera)
+        // {
+        //     // var screenAspect = camera.targetTexture.width / (float) camera.targetTexture.height;
+        //     // var widthFactor = Screen.width / (float) camera.targetTexture.width;
+        //     
+        //     var heightFactor = Screen.height / (float) camera.targetTexture.height;
+        //
+        //     // var widthFactor = 2;
+        //     // var heightFactor = 2;
+        //     
+        //     // var zoom = camera.orthographicSize * 2 * heightFactor;
+        //     // var zoom = 16f * heightFactor;
+        //     var dpi = 16f;
+        //     
+        //     var bounds = new Bounds(
+        //         camera.transform.position,
+        //         new Vector3(camera.targetTexture.width / dpi, camera.targetTexture.height / dpi, 0));
+        //     return bounds;
+        // }
     }
 }
