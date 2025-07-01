b9540d910592bd8d6e78d70fa0077cf04d13fb0d
Ariel Coppes <ariel.coppes@gmail.com>
2025-07-01 16:33:39 -0300

    support to generate lut texture from color array

--- Packages/com.gemserk.gamescore/DataAssets/ColorSet.cs
+++ Packages/com.gemserk.gamescore/DataAssets/ColorSet.cs
@@ -13,13 +13,21 @@ namespace Game.DataAssets
     {
         public static Texture2D CreateLutTexture(this ColorSet colorSet)
         {
-            var lutColors = new Color[colorSet.colors.Length];
+            return CreateLutTexture(colorSet.colors);
+        }
+        
+        public static Texture2D CreateLutTexture(Color[] colors)
+        {
+            var lutColors = new Color[colors.Length];
                     
-            Array.Copy(colorSet.colors, lutColors, lutColors.Length);
+            Array.Copy(colors, lutColors, lutColors.Length);
                     
-            var lutTexture = new Texture2D(lutColors.Length, 1, TextureFormat.ARGB32, false);
-            lutTexture.filterMode = FilterMode.Point;
-            lutTexture.wrapMode = TextureWrapMode.Clamp;
+            var lutTexture = new Texture2D(lutColors.Length, 1, TextureFormat.ARGB32, false)
+            {
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp
+            };
+            
             lutTexture.SetPixels(lutColors);
             lutTexture.Apply();
             
