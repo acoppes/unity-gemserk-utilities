38c901ff93319c7eb2e54d6eea4a73770d70ab5f
Ariel Coppes <ariel.coppes@gmail.com>
2025-07-25 10:29:44 -0300

    added a cancelhandler unity delegate

---
+++ Packages/com.gemserk.utilities/UI/CancelHandlerUnityEventDelegate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Gemserk.Utilities.UI
+{
+    public class CancelHandlerUnityEventDelegate : MonoBehaviour, ICancelHandler
+    {
+        public UnityEvent onCancel;
+        
+        public void OnCancel(BaseEventData eventData)
+        {
+            onCancel.Invoke();
+        }
+    }
+}
