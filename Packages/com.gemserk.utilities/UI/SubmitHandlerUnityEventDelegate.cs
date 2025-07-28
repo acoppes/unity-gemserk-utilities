8f7c8bdfae5678d37b3a6062e9df201114ab268f
Ariel Coppes <ariel.coppes@gmail.com>
2025-07-28 11:46:09 -0300

    added new submit handler to unity event delegate

---
+++ Packages/com.gemserk.utilities/UI/SubmitHandlerUnityEventDelegate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Gemserk.Utilities.UI
+{
+    public class SubmitHandlerUnityEventDelegate : MonoBehaviour, ISubmitHandler
+    {
+        public UnityEvent onSubmit;
+
+        public void OnSubmit(BaseEventData eventData)
+        {
+            onSubmit.Invoke();
+        }
+    }
+}
