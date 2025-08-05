70a3aeccf83cd3ea6857137ee8ec9328cce5dc52
Ariel Coppes <ariel.coppes@gmail.com>
2025-08-05 09:46:04 -0300

    added new delegate for select/deselect handlers to unity events

---
+++ Packages/com.gemserk.utilities/UI/SelectionHandlerUnityEventDelegate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Gemserk.Utilities.UI
+{
+    public class SelectionHandlerUnityEventDelegate : MonoBehaviour, ISelectHandler, IDeselectHandler
+    {
+        public UnityEvent onSelected, onDeselected;
+        
+        public void OnSelect(BaseEventData eventData)
+        {
+            onSelected.Invoke();
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            onDeselected.Invoke();
+        }
+    }
+}
