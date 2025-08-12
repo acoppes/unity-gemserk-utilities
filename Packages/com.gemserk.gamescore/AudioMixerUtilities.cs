e5ba38d0cfd30e103738c91902f8015f8af49203
Ariel Coppes <ariel.coppes@gmail.com>
2025-08-12 09:55:48 -0300

    added new methods to set audiomixer using min/max values if we want to set a different range of max value

--- Packages/com.gemserk.gamescore/AudioMixerUtilities.cs
+++ Packages/com.gemserk.gamescore/AudioMixerUtilities.cs
@@ -13,5 +13,17 @@ namespace Game
         {
             return Mathf.Log(value) * 20;
         }
+        
+        public static float ConvertFromNormalizedValueToDb(float value, float min, float max)
+        {
+            var newValue = Mathf.Lerp(min, max,value);
+            return Mathf.Log(newValue) * 20;
+        }
+        
+        public static float ConvertFromDbToNormalizedValue(float dbValue, float min, float max)
+        {
+            var storedValue = Mathf.Exp(dbValue / 20f);
+            return Mathf.InverseLerp(min, max, storedValue);
+        }
     }
 }
