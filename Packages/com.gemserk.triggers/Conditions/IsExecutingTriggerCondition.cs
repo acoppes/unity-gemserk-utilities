a845287a3169d108d9580f1dab494999982fbd00
Ariel Coppes <ariel.coppes@gmail.com>
2025-08-26 19:29:36 -0300

    added condition to check if trigger is executing

---
+++ Packages/com.gemserk.triggers/Conditions/IsExecutingTriggerCondition.cs
@@ -0,0 +1,22 @@
+﻿namespace Gemserk.Triggers.Conditions
+{
+    public class IsExecutingTriggerCondition : TriggerCondition
+    {
+        private ITrigger trigger;
+        
+        private void Awake()
+        {
+            trigger = GetComponentInParent<ITrigger>();
+        }
+
+        public override string GetObjectName()
+        {
+            return "IsExecuting()";
+        }
+        
+        public override bool Evaluate(object activator = null)
+        {
+            return trigger.State == ITrigger.ExecutionState.Executing;
+        }
+    }
+}
