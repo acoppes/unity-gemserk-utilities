79298bce6b25e21e6aa8101f36a91452cb276462
Ariel Coppes <ariel.coppes@gmail.com>
2025-10-15 11:13:17 -0300

    started integrating the int lock idea we used in older projects in order to have stackable bools

---
+++ Packages/com.gemserk.utilities/IntLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Gemserk.Utilities
+{
+    public struct IntLock
+    {
+        private int intLock;
+
+        public bool value {
+            get => intLock == 0;
+            set {
+                intLock = value ? intLock - 1 : intLock + 1;
+                if (intLock < 0)
+                    throw new Exception("Lock should never be negative");
+            }
+        }
+
+        public bool locked
+        {
+            get => intLock > 0;
+            set
+            {
+                intLock = value ? intLock + 1 : intLock - 1;
+                if (intLock < 0)
+                    throw new Exception("Lock should never be negative");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"V:{value.ToString()}, L:{intLock}";
+        }
+    }
+}
