ec7a8d9eaf1c9dd6fef8438b7d6473ff5e0df26d
Ariel Coppes <ariel.coppes@gmail.com>
2026-01-01 14:21:10 -0300

    now statscomponent def already adds the statsmodifier component

--- Packages/com.gemserk.gamescore/Components/StatsComponentDefinition.cs
+++ Packages/com.gemserk.gamescore/Components/StatsComponentDefinition.cs
@@ -70,7 +70,157 @@ namespace Game.Components
     //     
     //     public float add;
     //     public float mult;
-    // }
+    // 
+    
+     [Serializable]
+    public class StatModifierDefinition
+    {
+        public IntTypeAsset statType;
+        public float add = 0f;
+        
+        public float mult = 1f;
+
+        public StatModifier CreateModifier()
+        {
+            return new StatModifier()
+            {
+                type = statType.value,
+                add = add,
+                mult = mult
+            };
+        }
+    }
+    
+    [Serializable]
+    public class StatsModifierDefinition
+    {
+        public IntTypeAsset modifierType;
+        public float time;
+        
+        public List<StatModifierDefinition> statsModifiers;
+
+        public StatsModifier CreateModifier()
+        {
+            var statsModifiersData = StatsModifier.Default();
+            statsModifiersData.type = modifierType.value;
+            statsModifiersData.time = time;
+
+            foreach (var statModifierDefinition in statsModifiers)
+            {
+                statsModifiersData.modifiers[statModifierDefinition.statType.value] = statModifierDefinition.CreateModifier();
+            }
+
+            return statsModifiersData;
+        }
+    }
+    
+    public struct StatModifier
+    {
+        public const int Undefined = -1;
+        
+        public int type;
+        public float add;
+        public float mult;
+    }
+    
+    public struct StatsModifier
+    {
+        public const int Undefined = -1;
+        
+        public enum State
+        {
+            Inactive,
+            Refresh,
+            Active
+        }
+
+        public int type;
+
+        public State previousState;
+        public State state;
+        
+        public StatModifier[] modifiers;
+        
+        public float time;
+        public float currentTime;
+        public float activeTime;
+
+        public bool active => time > 0 && currentTime < time;
+
+        public bool remove;
+
+        public static StatsModifier Default()
+        {
+            var statsModifier = new StatsModifier
+            {
+                type = Undefined,
+                state = State.Inactive,
+                modifiers = new StatModifier[StatsComponent.MaxStats],
+                currentTime = 0,
+                time = 0,
+                previousState = State.Inactive
+            };
+
+            for (var i = 0; i < statsModifier.modifiers.Length; i++)
+            {
+                statsModifier.modifiers[i] = new StatModifier()
+                {
+                    type = StatModifier.Undefined,
+                    add = 0,
+                    mult = 1
+                };
+            }
+
+            return statsModifier;
+        }
+
+        public ref StatModifier Get(int statType)
+        {
+            return ref modifiers[statType];
+        }
+    }
+    
+    public struct StatsModifiersComponent : IEntityComponent
+    {
+        public static int MaxModifiers = 16;
+        
+        public StatsModifier[] statsModifiers;
+
+        public bool pendingRecalculateStats;
+        
+        public static StatsModifiersComponent Default()
+        {
+            var statsModifiers = new StatsModifier[MaxModifiers];
+
+            for (var i = 0; i < statsModifiers.Length; i++)
+            {
+                statsModifiers[i] = StatsModifier.Default();
+            }
+
+            return new StatsModifiersComponent()
+            {
+                statsModifiers = statsModifiers,
+                pendingRecalculateStats = true
+            };
+        }
+
+        public void Add(StatsModifier modifier)
+        {
+            // should I keep a separated list of pending modifiers to be added instead and process in system?
+            
+            // this will copy pointers to other arrays
+            var currentModifier = modifier;
+            
+            currentModifier.state = StatsModifier.State.Refresh;
+            
+            statsModifiers[modifier.type] = currentModifier;
+        }
+
+        public void Remove(int modifierType)
+        {
+            statsModifiers[modifierType].remove = true;
+        }
+    }
 
     public class StatsComponentDefinition : ComponentDefinitionBase
     {
@@ -87,6 +237,7 @@ namespace Game.Components
         public override void Apply(World world, Entity entity)
         {
             var stats = StatsComponent.Default();
+            
             foreach (var statDefinition in statDefinitions)
             {
                 stats.SetStat(new Stat()
@@ -96,7 +247,9 @@ namespace Game.Components
                     value = statDefinition.value
                 });
             }
+            
             entity.Add(stats);
+            entity.Add(StatsModifiersComponent.Default());
             
             // Should add automatically th estats modifier component too here?
             
