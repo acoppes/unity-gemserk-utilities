0f4bf5ee43b3a967f94f89f35a5b580272786c0b
Ariel Coppes <ariel.coppes@gmail.com>
2026-01-08 10:08:54 -0300

    improved system by caching the filter with ecs inject filter

--- Packages/com.gemserk.gamescore/Systems/DestroyEntityFromGameObjectSystem.cs
+++ Packages/com.gemserk.gamescore/Systems/DestroyEntityFromGameObjectSystem.cs
@@ -1,25 +1,24 @@
 ﻿using Gemserk.Leopotam.Ecs;
 using Gemserk.Leopotam.Ecs.Components;
+using Gemserk.Leopotam.Ecs.Controllers;
 using Gemserk.Utilities.Pooling;
 using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
 
 namespace Game.Systems
 {
     public class DestroyEntityFromGameObjectSystem : BaseSystem, IEcsRunSystem
     {
+        readonly EcsFilterInject<Inc<GameObjectComponent, DestroyableComponent>> destroyablesWithGameObject = default;
+        
         public void Run(EcsSystems systems)
         {
-            var gameObjectComponents = world.GetComponents<GameObjectComponent>();
-            var destroyableComponents = world.GetComponents<DestroyableComponent>();
-
-            foreach (var entity in world.GetFilter<GameObjectComponent>()
-                         .Inc<DestroyableComponent>()
-                         .End())
+            foreach (var e in destroyablesWithGameObject.Value)
             {
-                var gameObjectComponent = gameObjectComponents.Get(entity);
-                ref var destroyableComponent = ref destroyableComponents.Get(entity);
+                ref var gameObjectComponent = ref destroyablesWithGameObject.Pools.Inc1.Get(e);
+                ref var destroyableComponent = ref destroyablesWithGameObject.Pools.Inc2.Get(e);
 
-                if (gameObjectComponent.gameObject == null)
+                if (!gameObjectComponent.gameObject)
                 {
                     destroyableComponent.destroy = true;
                 }
