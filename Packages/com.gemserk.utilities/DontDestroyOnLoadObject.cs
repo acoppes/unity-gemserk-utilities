2e01cb49cedf0d533c858e8bfbec4d035999cc0e
Ariel Coppes <ariel.coppes@gmail.com>
2026-01-20 09:56:30 -0300

    option to automove to root when using dont destroy on looad

--- Packages/com.gemserk.utilities/DontDestroyOnLoadObject.cs
+++ Packages/com.gemserk.utilities/DontDestroyOnLoadObject.cs
@@ -4,8 +4,14 @@ namespace Gemserk.Utilities
 {
     public class DontDestroyOnLoadObject : MonoBehaviour
     {
+        public bool autoMoveToRoot;
+        
         private void Awake()
         {
+            if (autoMoveToRoot)
+            {
+                gameObject.transform.SetParent(null);
+            }
             DontDestroyOnLoad(gameObject);
         }
     }
