75d821f18b17f80436b77da3dcc9e908d41f10b8
Ariel Coppes <ariel.coppes@gmail.com>
2026-02-06 17:51:00 -0300

    just more ui mockups for new debug state window with ui elements

--- Packages/com.gemserk.triggers/Editor/NewTriggersDebugStateWindow.cs
+++ Packages/com.gemserk.triggers/Editor/NewTriggersDebugStateWindow.cs
@@ -6,25 +6,56 @@ public class NewTriggersDebugStateWindow : EditorWindow
 {
     [SerializeField]
     private VisualTreeAsset m_VisualTreeAsset = default;
+    
+    [SerializeField]
+    private VisualTreeAsset m_VisualElementTemplate = default;
 
     // [MenuItem("Window/UI Toolkit/NewTriggersDebugStateWindow")]
     // public static void ShowExample()
     // {
-    //     NewTriggersDebugStateWindow wnd = GetWindow<NewTriggersDebugStateWindow>();
+    //     var wnd = GetWindow<NewTriggersDebugStateWindow>();
     //     wnd.titleContent = new GUIContent("NewTriggersDebugStateWindow");
     // }
 
     public void CreateGUI()
     {
         // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
+        var root = rootVisualElement;
 
         // VisualElements objects can contain other VisualElement following a tree hierarchy.
         // VisualElement label = new Label("Hello World! From C#");
         // root.Add(label);
-
+        
         // Instantiate UXML
-        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(labelFromUXML);
+        var fromXml = m_VisualTreeAsset.Instantiate();
+        // root.Add(fromXml);
+        // return;
+        
+        // var triggerElementTemplate = fromXml.Query<VisualElement>("TriggerElement-Template").First();
+        
+        var mainScroll = fromXml.Query<VisualElement>("MainScroll").First();
+        root.Add(mainScroll);
+
+        for (int i = 0; i < 50; i++)
+        {
+            var custom = m_VisualElementTemplate.Instantiate();
+            if (i % 2 == 0)
+            {
+                custom.Q("TriggerElement").AddToClassList("trigger-even");
+            }
+
+            custom.Q<Foldout>().value = false;
+            custom.Q<Label>().text = "MY CUSTOM TEXT 1";
+            mainScroll.Add(custom);
+        }
+        
+        // var custom1 = m_VisualElementTemplate.Instantiate();
+        // custom1.Q<Label>().text = "MY CUSTOM TEXT 1";
+        // mainScroll.Add(custom1);
+        //
+        // var custom2 = m_VisualElementTemplate.Instantiate();
+        //
+        // custom2.Q<Label>().text = "MY CUSTOM TEXT 2";
+        // mainScroll.Add(custom2);
     }
 }
