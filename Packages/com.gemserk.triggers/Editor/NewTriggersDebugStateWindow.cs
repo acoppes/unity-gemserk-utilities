d2ad4f77f4e9aac02604f8c4711389661da82369
Ariel Coppes <ariel.coppes@gmail.com>
2026-02-07 19:12:04 -0300

    more work on the trigger window using ui elements version

--- Packages/com.gemserk.triggers/Editor/NewTriggersDebugStateWindow.cs
+++ Packages/com.gemserk.triggers/Editor/NewTriggersDebugStateWindow.cs
@@ -10,12 +10,12 @@ public class NewTriggersDebugStateWindow : EditorWindow
     [SerializeField]
     private VisualTreeAsset m_VisualElementTemplate = default;
 
-    // [MenuItem("Window/UI Toolkit/NewTriggersDebugStateWindow")]
-    // public static void ShowExample()
-    // {
-    //     var wnd = GetWindow<NewTriggersDebugStateWindow>();
-    //     wnd.titleContent = new GUIContent("NewTriggersDebugStateWindow");
-    // }
+    [MenuItem("Window/UI Toolkit/NewTriggersDebugStateWindow")]
+    public static void ShowExample()
+    {
+        var wnd = GetWindow<NewTriggersDebugStateWindow>();
+        wnd.titleContent = new GUIContent("NewTriggersDebugStateWindow");
+    }
 
     public void CreateGUI()
     {
@@ -36,16 +36,56 @@ public class NewTriggersDebugStateWindow : EditorWindow
         var mainScroll = fromXml.Query<VisualElement>("MainScroll").First();
         root.Add(mainScroll);
 
+        var executeGuiContent = new GUIContent(EditorGUIUtility.IconContent("d_PlayButton").image)
+        {
+            tooltip = "Queue Execution"
+        };
+        
+        var forceExecuteGuiContent = new GUIContent(EditorGUIUtility.IconContent("d_StepButton").image)
+        {
+            tooltip = "Force Execution"
+        };
+        
+        var expandGuiContent = new GUIContent(EditorGUIUtility.IconContent("FolderOpened On Icon").image)
+        {
+            tooltip = "Toggle Expand"
+        };
+        
         for (int i = 0; i < 50; i++)
         {
             var custom = m_VisualElementTemplate.Instantiate();
+
+            var element = custom.Q("TriggerElement");
+            
             if (i % 2 == 0)
             {
-                custom.Q("TriggerElement").AddToClassList("trigger-even");
+                element.AddToClassList("trigger-even");
             }
 
-            custom.Q<Foldout>().value = false;
-            custom.Q<Label>().text = "MY CUSTOM TEXT 1";
+            // custom.Q<Foldout>().value = false;
+            
+            element.Q<Label>().text = "MY CUSTOM TEXT 1";
+            var buttonExecute = element.Q<Button>("ButtonExecute");
+            buttonExecute.clicked += () =>
+            {
+                Debug.Log("EXECUTE");
+            };
+            buttonExecute.Q<Image>().image = executeGuiContent.image;
+            
+            var buttonForceExecute = element.Q<Button>("ButtonForceExecute");
+            buttonForceExecute.clicked += () =>
+            {
+                Debug.Log("FORCE EXECUTE");
+            };
+            buttonForceExecute.Q<Image>().image = forceExecuteGuiContent.image; 
+            
+            var buttonExpand = element.Q<Button>("ButtonExpand");
+            buttonExpand.clicked += () =>
+            {
+                Debug.Log("EXPAND ON/OFF");
+            };
+            buttonExpand.Q<Image>().image = expandGuiContent.image; 
+            
             mainScroll.Add(custom);
         }
         
