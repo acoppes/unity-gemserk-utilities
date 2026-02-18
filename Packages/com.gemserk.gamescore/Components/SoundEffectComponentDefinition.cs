f0b74f277f6578b5ce81042a2813cf8c1da2a96e
Ariel Coppes <ariel.coppes@gmail.com>
2026-02-18 10:14:52 -0300

    instantiate trigger action can spawn from definitions now as option

--- Packages/com.gemserk.gamescore/Components/SoundEffectComponentDefinition.cs
+++ Packages/com.gemserk.gamescore/Components/SoundEffectComponentDefinition.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Gemserk.Leopotam.Ecs;
 using MyBox;
 using UnityEngine;
--- Packages/com.gemserk.triggers.extensions/InstantiateEntityPrefabInstance.cs
+++ Packages/com.gemserk.triggers.extensions/InstantiateEntityPrefabInstance.cs
@@ -1,38 +1,72 @@
 ﻿using Gemserk.Leopotam.Ecs;
 using Gemserk.Utilities;
+using MyBox;
 using UnityEngine;
 
 namespace Gemserk.Triggers
 {
-    public class InstantiateEntityPrefabInstance : TriggerAction
+    public class InstantiateEntityPrefabInstance : WorldTriggerAction
     {
+        public enum SourceType
+        {
+            FromEntityPrefab = 0,
+            FromDefinition = 1
+        }
+
+        public SourceType sourceType = SourceType.FromEntityPrefab;
+        
+        [ConditionalField(nameof(sourceType), false, SourceType.FromEntityPrefab)]
         [ObjectType(typeof(BaseEntityPrefabInstance), disableAssetReferences = true, disablePrefabReferences = true)]
         public Object entityInstance;
 
+        [ConditionalField(nameof(sourceType), false, SourceType.FromDefinition)]
+        [ObjectType(typeof(IEntityDefinition), disableAssetReferences = true, disablePrefabReferences = false)]
+        public Object entityDefinition;
+        
         public override string GetObjectName()
         {
-            if (entityInstance)
+            if (sourceType == SourceType.FromEntityPrefab)
             {
-                return $"InstantiateEntity({entityInstance.name})";
+                if (entityInstance)
+                {
+                    return $"InstantiateEntity{sourceType}({entityInstance.name})";
+                }
+            } else if (sourceType == SourceType.FromDefinition)
+            {
+                if (entityDefinition)
+                {
+                    return $"InstantiateEntity{sourceType}({entityDefinition.name})";
+                }
             }
             return "InstantiateEntity()";
         }
 
         public override ITrigger.ExecutionResult Execute(object activator = null)
         {
-            if (!entityInstance)
+            if (sourceType == SourceType.FromEntityPrefab)
             {
-                return ITrigger.ExecutionResult.Completed;
-            }
+                if (!entityInstance)
+                {
+                    return ITrigger.ExecutionResult.Completed;
+                }
 
-            var entityPrefabInstance = entityInstance.GetInterface<BaseEntityPrefabInstance>();
+                var entityPrefabInstance = entityInstance.GetInterface<BaseEntityPrefabInstance>();
             
-            if (!entityPrefabInstance)
+                if (!entityPrefabInstance)
+                {
+                    return ITrigger.ExecutionResult.Completed;
+                }
+            
+                entityPrefabInstance.InstantiateEntity();
+            } else if (sourceType == SourceType.FromDefinition)
             {
-                return ITrigger.ExecutionResult.Completed;
+                if (!entityDefinition)
+                {
+                    return ITrigger.ExecutionResult.Completed;
+                }
+
+                world.CreateEntity(entityDefinition);
             }
-            
-            entityPrefabInstance.InstantiateEntity();
             
             return ITrigger.ExecutionResult.Completed;
         }
