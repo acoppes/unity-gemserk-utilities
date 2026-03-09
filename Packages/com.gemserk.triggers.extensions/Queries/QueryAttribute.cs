1fad34970112ecc98883e3e16fa240633da15f9c
Ariel Coppes <ariel.coppes@gmail.com>
2026-03-09 14:19:30 -0300

    query attribute to simplify configuring objecttype for queries

---
+++ Packages/com.gemserk.triggers.extensions/Queries/QueryAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Gemserk.Utilities;
+
+namespace Gemserk.Triggers.Queries
+{
+    public class QueryAttribute : ObjectTypeAttribute
+    {
+        public QueryAttribute() : base(typeof(Query))
+        {
+            filterString = "Query_";
+            disableAssetReferences = true;
+            sceneReferencesOnWhenStart = true;
+            prefabReferencesOnWhenStart = true;
+        }
+    }
+}
