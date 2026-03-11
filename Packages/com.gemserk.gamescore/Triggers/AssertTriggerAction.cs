b391d1ab5b6dd3191f21edd0e9c97c21415e611d
Ariel Coppes <ariel.coppes@gmail.com>
2026-03-11 11:56:24 -0300

    added assert action to check for a condition and fail with some error if condition is false, useful for automatic tests

---
+++ Packages/com.gemserk.gamescore/Triggers/AssertTriggerAction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+using UnityEngine.Assertions;
+
+namespace Game.Triggers
+{
+    public class AssertTriggerAction : WorldTriggerAction
+    {
+        public TriggerTarget triggerTarget;
+
+        public TriggerCondition condition;
+        
+        public override string GetObjectName()
+        {
+            if (condition)
+            {
+                return $"Assert{condition.GetObjectName()}";
+            }
+            return "Assert()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = new List<Entity>();
+            
+            world.GetTriggerTargetEntities(null, triggerTarget, activator, entities);
+
+            if (entities.Count > 0)
+            {
+                foreach (var entity in entities)
+                {
+                    // if (!condition.Evaluate(entity))
+                    // {
+                    //     
+                    // }
+                    Assert.IsTrue(condition.Evaluate(entity), $"{condition.GetObjectName()} failed on {entity}");
+                } 
+            }
+            else
+            {
+                Assert.IsTrue(condition.Evaluate(activator), $"{condition.GetObjectName()} failed on {activator}");
+            }
+
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
