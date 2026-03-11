c1fb364a3794d85abffdc8e801e28713613fbe4e
Ariel Coppes <ariel.coppes@gmail.com>
2026-03-11 11:55:53 -0300

    added unity editor trigger action to stop/pause the editor from triggers (only useful in editor/dev scenes)

---
+++ Packages/com.gemserk.triggers.extensions/UnityEditorTriggerAction.cs
@@ -0,0 +1,37 @@
+﻿namespace Gemserk.Triggers
+{
+    public class UnityEditorTriggerAction : TriggerAction
+    {
+        public enum ActionType
+        {
+            Stop = 0,
+            Pause = 1,
+        }
+
+        public ActionType actionType = ActionType.Stop;
+
+        public override string GetObjectName()
+        {
+            return $"UnityEditor.{actionType}()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            if (actionType == ActionType.Pause)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPaused = true;
+#endif
+            }
+            
+            if (actionType == ActionType.Stop)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            }
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
