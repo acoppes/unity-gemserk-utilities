f9bec7f26d42da6a4eaf01fb6192bf301a377f29
Ariel Coppes <ariel.coppes@gmail.com>
2026-03-12 09:47:26 -0300

    trigger to instantiate an entity from another's entity definition as a test

---
+++ Packages/com.gemserk.triggers.extensions/InstantiateFromEntityDefinitionTriggerAction.cs
@@ -0,0 +1,31 @@
+﻿using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers.Queries;
+using Gemserk.Utilities;
+using MyBox;
+using UnityEngine;
+
+namespace Gemserk.Triggers
+{
+    public class InstantiateFromEntityDefinitionTriggerAction : WorldTriggerAction
+    {
+        public TriggerTarget target;
+        
+        public override string GetObjectName()
+        {
+            return $"InstantiateFromDefinition({target})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var targets = world.GetEntities(target, activator);
+
+            foreach (var entityTarget in targets)
+            {
+                var definitionComponent = entityTarget.Get<EntityDefinitionComponent>();
+                world.CreateEntity(definitionComponent.definition, definitionComponent.parameters);
+            }
+            
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
