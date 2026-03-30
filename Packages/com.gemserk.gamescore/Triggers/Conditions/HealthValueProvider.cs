f0c84769a5c3402f41d83ca9223925a6dc72a763
Ariel Coppes <ariel.coppes@gmail.com>
2026-03-30 12:12:35 -0300

    health value provider can check for total or current now

--- Packages/com.gemserk.gamescore/Triggers/Conditions/HealthValueProvider.cs
+++ Packages/com.gemserk.gamescore/Triggers/Conditions/HealthValueProvider.cs
@@ -6,9 +6,15 @@ namespace Game.Triggers.Conditions
 {
     public class HealthValueProvider : MonoBehaviour, IValueProvider
     {
+        public bool useTotal;
+        
         public float GetValue(World world, object activator)
         {
             var entity = (Entity)activator;
+            if (useTotal)
+            {
+                return entity.Get<HealthComponent>().total;
+            }
             return entity.Get<HealthComponent>().current;
         }
     }
