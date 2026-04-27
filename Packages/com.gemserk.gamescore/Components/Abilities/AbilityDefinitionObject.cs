796b8a26e52fe9616f0895bdd5282ab89e9b41c1
Ariel Coppes <ariel.coppes@gmail.com>
2026-04-27 13:48:32 -0300

    force set gameobject name on validate

--- Packages/com.gemserk.gamescore/Components/Abilities/AbilityDefinitionObject.cs
+++ Packages/com.gemserk.gamescore/Components/Abilities/AbilityDefinitionObject.cs
@@ -5,5 +5,10 @@ namespace Game.Components.Abilities
     public class AbilityDefinitionObject : MonoBehaviour
     {
         public AbilityDefinition abilityDefinition;
+
+        private void OnValidate()
+        {
+            gameObject.name = abilityDefinition.name;
+        }
     }
 }
