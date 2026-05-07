fe2b142b5681a9af5bb5e90ca104e17ca5082681
Ariel Coppes <ariel.coppes@gmail.com>
2026-05-07 11:56:28 -0300

    added trigger to stop/quit application

---
+++ Packages/com.gemserk.triggers.extensions/ApplicationTriggerAction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Gemserk.Triggers
+{
+    public class ApplicationTriggerAction : TriggerAction
+    {
+        public enum ActionType
+        {
+            Quit = 0
+        }
+
+        public ActionType actionType = ActionType.Quit;
+
+        public override string GetObjectName()
+        {
+            return $"Application.{actionType}()";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            Application.Quit();
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
