32c1e6c8a1161fab073a5b8164d49cece7395fd6
Ariel Coppes <ariel.coppes@gmail.com>
2026-05-11 21:53:49 -0300

    added optional events for window open start and window close start

--- Packages/com.gemserk.utilities/UI/UIWindowAnimationEvents.cs
+++ Packages/com.gemserk.utilities/UI/UIWindowAnimationEvents.cs
@@ -1,21 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Gemserk.Utilities.UI
 {
     public class UIWindowAnimationEvents : MonoBehaviour
     {
+        public UnityEvent onOpenAnimationStarted;
         public UnityEvent onOpenAnimationCompleted;
+        
+        public UnityEvent onClosedAnimationStarted;
         public UnityEvent onClosedAnimationCompleted;
+
+        [NonSerialized]
+        public bool animating;
+        
+        public void OnWindowOpenStarted()
+        {
+            onOpenAnimationStarted.Invoke();
+            animating = true;
+        }
         
         public void OnWindowOpened()
         {
             onOpenAnimationCompleted.Invoke();
+            animating = false;
+        }
+        
+        public void OnWindowCloseStarted()
+        {
+            onClosedAnimationStarted.Invoke();
+            animating = true;
         }
 
         public void OnWindowClosed()
         {
             onClosedAnimationCompleted.Invoke();
+            animating = false;
         }
     }
 }
