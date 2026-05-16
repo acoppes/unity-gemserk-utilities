416b73d92c5f8172eab54ac8faef9755b322ec0b
Ariel Coppes <ariel.coppes@gmail.com>
2026-05-15 22:42:32 -0300

    trigger to switch player input on the run but might be not needed in the end

---
+++ Packages/com.gemserk.gamescore/Triggers/Actions/PlayerInputTriggerAction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Triggers;
+
+namespace Game.Triggers.Actions
+{
+    public class PlayerInputTriggerAction : WorldTriggerAction
+    {
+        public enum ActionType
+        {
+            Switch = 0
+        }
+        
+        public TriggerTarget target;
+
+        public ActionType actionType = ActionType.Switch;
+
+        public int newPlayerInput;
+
+        public override string GetObjectName()
+        {
+            return $"PlayerInput{actionType}({target})";
+        }
+
+        public override ITrigger.ExecutionResult Execute(object activator = null)
+        {
+            var entities = new List<Entity>();
+            target.Get(entities, world, activator);
+            
+            foreach (var entity in entities)
+            {
+                ref var playerInput = ref world.GetComponent<PlayerInputComponent>(entity);
+                playerInput.playerInput = newPlayerInput;
+            }
+
+            return ITrigger.ExecutionResult.Completed;
+        }
+    }
+}
