6b1917c0d4047e4033f4947fc73875ae043c0e9c
Ariel Coppes <ariel.coppes@gmail.com>
2026-06-06 19:38:42 -0300

    added tool to force refresh all graphics to fix the pixel perfect after ui scale change in ship miner

---
+++ Packages/com.gemserk.utilities/UI/CanvasToolsDelegate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gemserk.Utilities.UI
+{
+    public class CanvasToolsDelegate : MonoBehaviour
+    {
+        public void ForceRefresh()
+        {
+            var graphicsArray = FindObjectsByType<Graphic>(FindObjectsInactive.Include);
+            
+            foreach (var graphic in graphicsArray)
+            {
+                graphic.SetAllDirty();
+            }
+        }
+
+    }
+}
