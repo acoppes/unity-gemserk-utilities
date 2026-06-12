a8584c4fbbed03f6407e4f8fa8990508cd4a9451
Ariel Coppes <ariel.coppes@gmail.com>
2026-06-12 11:30:59 -0300

    changed to have new nav handler delegate with more options

---
+++ Packages/com.gemserk.utilities/UI/NavigationHandlerDelegate.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Gemserk.Utilities.UI
+{
+    public class NavigationHandlerDelegate : MonoBehaviour, IMoveHandler
+    {
+        public enum DelegateType
+        {
+            Parent = 0,
+            Object = 1
+        }
+
+        public DelegateType delegateType;
+        
+        // [ConditionalField(nameof(delegateType), false, DelegateType.Object)]
+        public GameObject delegateObject;
+        
+        public void OnMove(AxisEventData eventData)
+        {
+            var selectable = GetComponent<Selectable>();
+
+            if (selectable)
+            {
+                if (selectable.navigation.mode.HasFlag(Navigation.Mode.Horizontal))
+                {
+                    if (eventData.moveDir is MoveDirection.Left or MoveDirection.Right)
+                    {
+                        return;
+                    }
+                }
+                
+                if (selectable.navigation.mode.HasFlag(Navigation.Mode.Vertical))
+                {
+                    if (eventData.moveDir is MoveDirection.Down or MoveDirection.Up)
+                    {
+                        return;
+                    }
+                }
+            }
+            
+            if (delegateType == DelegateType.Parent)
+            {
+                ExecuteEvents.ExecuteHierarchy(transform.parent.gameObject, eventData, ExecuteEvents.moveHandler);
+            }
+            else if (delegateType == DelegateType.Object)
+            {
+                ExecuteEvents.ExecuteHierarchy(delegateObject, eventData, ExecuteEvents.moveHandler);
+            }
+        }
+    }
+}
