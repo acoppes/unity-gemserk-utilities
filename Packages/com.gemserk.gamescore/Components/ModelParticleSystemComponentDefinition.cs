32d8db2237774b57f9dd3be41070a5cac6d08767
Ariel Coppes <ariel.coppes@gmail.com>
2026-07-23 10:02:11 -0300

    added a more general way of reusing model for particles

---
+++ Packages/com.gemserk.gamescore/Components/ModelParticleSystemComponentDefinition.cs
@@ -0,0 +1,29 @@
+﻿using Game.Models;
+using Gemserk.Leopotam.Ecs;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Game.Components
+{
+    public struct ModelParticleSystemComponent : IEntityComponent
+    {
+        public GameObject particleSystemPrefab;
+        public GameObject instance;
+        public Model currentModel;
+    }
+    
+    public class ModelParticleSystemComponentDefinition : ComponentDefinitionBase
+    {
+        public GameObject particleSystemPrefab;
+
+        public override void Apply(World world, Entity entity)
+        {
+            Assert.IsNotNull(particleSystemPrefab, $"{gameObject.name}: ModelComponent needs prefab to work");
+            
+            world.AddComponent(entity, new ModelParticleSystemComponent
+            {
+                particleSystemPrefab = particleSystemPrefab
+            });
+        }
+    }
+}
---
+++ Packages/com.gemserk.gamescore/Systems/ModelParticlesCreationSystem.cs
@@ -0,0 +1,60 @@
+﻿using Game.Components;
+using Gemserk.Leopotam.Ecs;
+using Gemserk.Utilities.Pooling;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Game.Systems
+{
+    public class ModelParticlesCreationSystem : BaseSystem, IEntityCreatedHandler, IEntityDestroyedHandler,
+        IEcsInitSystem
+    {
+        private GameObjectPoolMap particlesPoolMap;
+        
+        public void Init(EcsSystems systems)
+        {
+            particlesPoolMap = new GameObjectPoolMap("~ParticleModelsPool");
+        }
+        
+        public void OnEntityCreated(World world, Entity entity)
+        {
+            if (entity.Has<ModelInstanceComponent>() && entity.Has<ModelParticleSystemComponent>())
+            {
+                ref var modelParticle = ref entity.Get<ModelParticleSystemComponent>();
+                modelParticle.instance = particlesPoolMap.Get(modelParticle.particleSystemPrefab);
+                
+                var instance = entity.Get<ModelInstanceComponent>().instance;
+                instance.particleSystem = modelParticle.instance.GetComponent<ParticleSystem>();
+                instance.model = modelParticle.instance.transform;
+            
+                modelParticle.instance.transform.SetParent(instance.transform, false);
+                modelParticle.currentModel = instance;
+            }
+        }
+
+        public void OnEntityDestroyed(World world, Entity entity)
+        {
+            if (entity.Has<ModelParticleSystemComponent>())
+            {
+                ref var modelParticle = ref entity.Get<ModelParticleSystemComponent>();
+
+                if (modelParticle.currentModel)
+                {
+                    modelParticle.currentModel.particleSystem = null;
+                    modelParticle.currentModel.model = null;
+                }
+                
+                if (modelParticle.instance)
+                {
+                    modelParticle.instance.transform.SetParent(null, false);
+                    particlesPoolMap.Release(modelParticle.particleSystemPrefab, modelParticle.instance);
+                }
+                
+                modelParticle.currentModel = null;
+                modelParticle.instance = null;
+            }
+        }
+
+
+    }
+}
