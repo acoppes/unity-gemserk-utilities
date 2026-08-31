ebaf8e3421a678677555bcc3a29a5e311cf0a909
Ariel Coppes <ariel.coppes@gmail.com>
2026-08-31 11:53:40 -0300

    added scene manager extension methods

---
+++ Packages/com.gemserk.utilities/SceneManagerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Gemserk.Utilities
+{
+    public static class SceneManagerExtensions
+    {
+        public static List<T> FindObjectsInAllScenes<T>(bool includeInactive)
+        {
+            var collected = new List<T>();
+            
+            var sceneCount = SceneManager.sceneCount;
+
+            for (var i = 0; i < sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                var rootGameObjects = scene.GetRootGameObjects();
+                foreach (var root in rootGameObjects)
+                {
+                    var values = root.GetComponentsInChildren<T>(includeInactive);
+                    collected.AddRange(values);
+                }
+            }
+
+            return collected;
+        }
+    }
+}
