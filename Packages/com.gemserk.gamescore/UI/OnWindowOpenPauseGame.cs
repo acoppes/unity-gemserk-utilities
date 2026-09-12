2773c668f70730ef71a7ae499ce2a40539d746c6
Ariel Coppes <ariel.coppes@gmail.com>
2026-09-12 19:25:27 -0300

    moved on window open pause game to core

---
+++ Packages/com.gemserk.gamescore/UI/OnWindowOpenPauseGame.cs
@@ -0,0 +1,38 @@
+﻿using Gemserk.Utilities.UI;
+using UnityEngine;
+
+namespace Game.UI
+{
+    public class OnWindowOpenPauseGame : MonoBehaviour
+    {
+        public UIWindow window;
+        
+        private void OnEnable()
+        {
+            if (window)
+            {
+                window.onOpenAction.AddListener(OnWindowOpen);
+                window.onCloseAction.AddListener(OnWindowClosed);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (window)
+            {
+                window.onOpenAction.RemoveListener(OnWindowOpen);
+                window.onCloseAction.RemoveListener(OnWindowClosed);
+            }
+        }
+
+        private static void OnWindowOpen()
+        {
+            GamePause.Pause();
+        }
+        
+        private static void OnWindowClosed()
+        {
+            GamePause.Resume();
+        }
+    }
+}
