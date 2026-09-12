36c04a93d484d05c6f1996b4d61c3958d012c771
Ariel Coppes <ariel.coppes@gmail.com>
2026-09-12 19:27:47 -0300

    moved autoselect on window open feature from ship miner

---
+++ Packages/com.gemserk.gamescore/UI/SelectObjectOnWindowOpen.cs
@@ -0,0 +1,56 @@
+﻿using Gemserk.Utilities.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Game.UI
+{
+    public class SelectObjectOnWindowOpen : MonoBehaviour
+    {
+        public UIWindow window;
+        public GameObject objectToAutoSelect;
+
+        private GameObject previousSelectedObject;
+
+        private void Awake()
+        {
+            if (window)
+            {
+                window.onOpenAction.AddListener(OnWindowOpen);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (window)
+            {
+                window.onOpenAction.RemoveListener(OnWindowOpen);
+            }
+        }
+
+        private void OnWindowOpen()
+        {
+            if (!isActiveAndEnabled)
+            {
+                return;
+            }
+            
+            if (EventSystem.current)
+            {
+                previousSelectedObject = EventSystem.current.currentSelectedGameObject;
+            }
+            
+            StartCoroutine(InputEventSystemUtils.DelegateSelectionDelayed(objectToAutoSelect));
+        }
+        
+        public void TryRestorePreviouslySelectedObject()
+        {
+            if (previousSelectedObject)
+            {
+                if (EventSystem.current)
+                {
+                    EventSystem.current.SetSelectedGameObject(previousSelectedObject);
+                }
+            }
+        }
+    }
+}
