964bcd49ebd7cfa23bbfa2d34a2bd4ca8e7312b4
Ariel Coppes <ariel.coppes@gmail.com>
2026-09-20 14:18:27 -0300

    added collision exit callback too

--- Packages/com.gemserk.gamescore/Utilities/EntityCollision2dDelegate.cs
+++ Packages/com.gemserk.gamescore/Utilities/EntityCollision2dDelegate.cs
@@ -13,50 +13,96 @@ namespace Game.Utilities
         public Entity entity;
         
         public IEntityCollisionDelegate.CollisionHandler onCollisionEnter;
-        
+        public IEntityCollisionDelegate.CollisionHandler onCollisionExit;
+         
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (onCollisionEnter != null)
-            {
-                var entityReference = collision.gameObject.GetComponentInParent<EntityReference>();
-                var collisionEntity = Entity.NullEntity;
+            if (onCollisionEnter == null) 
+                return;
+            
+            var entityReference = collision.gameObject.GetComponentInParent<EntityReference>();
+            var collisionEntity = Entity.NullEntity;
         
-                if (entityReference)
-                {
-                    collisionEntity = entityReference.entity;
-                }
-                
-                onCollisionEnter(world, entity, new IEntityCollisionDelegate.EntityCollision
-                {
-                    entity = collisionEntity,
-                    collider2D = collision.collider,
-                    collision2D = collision
-                    // collision = collision
-                });
+            if (entityReference)
+            {
+                collisionEntity = entityReference.entity;
             }
+                
+            onCollisionEnter(world, entity, new IEntityCollisionDelegate.EntityCollision
+            {
+                entity = collisionEntity,
+                collider2D = collision.collider,
+                collision2D = collision
+                // collision = collision
+            });
         }
         
         private void OnTriggerEnter2D(Collider2D collider)
         {
-            if (onCollisionEnter != null)
-            {
-                var entityReference = collider.gameObject.GetComponentInParent<EntityReference>();
-                var collisionEntity = Entity.NullEntity;
+            if (onCollisionEnter == null)
+                return;
+            
+            var entityReference = collider.gameObject.GetComponentInParent<EntityReference>();
+            var collisionEntity = Entity.NullEntity;
 
-                if (entityReference != null)
-                {
-                    collisionEntity = entityReference.entity;
-                }
-                
-                onCollisionEnter(world, entity, new IEntityCollisionDelegate.EntityCollision
-                {
-                    entity = collisionEntity,
-                    isTrigger = true,
-                    collider2D = collider,
-                    collision2D = null
-                    // collision = collision
-                });
+            if (entityReference)
+            {
+                collisionEntity = entityReference.entity;
             }
+                
+            onCollisionEnter(world, entity, new IEntityCollisionDelegate.EntityCollision
+            {
+                entity = collisionEntity,
+                isTrigger = true,
+                collider2D = collider,
+                collision2D = null
+                // collision = collision
+            });
+        }
+        
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (onCollisionExit == null) 
+                return;
+            
+            var entityReference = collision.gameObject.GetComponentInParent<EntityReference>();
+            var collisionEntity = Entity.NullEntity;
+        
+            if (entityReference)
+            {
+                collisionEntity = entityReference.entity;
+            }
+                
+            onCollisionExit(world, entity, new IEntityCollisionDelegate.EntityCollision
+            {
+                entity = collisionEntity,
+                collider2D = collision.collider,
+                collision2D = collision
+                // collision = collision
+            });
+        }
+        
+        private void OnTriggerExit2D(Collider2D collider)
+        {
+            if (onCollisionExit == null)
+                return;
+            
+            var entityReference = collider.gameObject.GetComponentInParent<EntityReference>();
+            var collisionEntity = Entity.NullEntity;
+
+            if (entityReference)
+            {
+                collisionEntity = entityReference.entity;
+            }
+                
+            onCollisionExit(world, entity, new IEntityCollisionDelegate.EntityCollision
+            {
+                entity = collisionEntity,
+                isTrigger = true,
+                collider2D = collider,
+                collision2D = null
+                // collision = collision
+            });
         }
     }
 }
